770bbefb48636ca73a7c436292939c403c883021
Jakub Trllo <jakub.trllo@gmail.com>
2024-02-06 11:39:31 +0100

    created 'AddonsManager'

--- client/ayon_core/addon/__init__.py
+++ client/ayon_core/addon/__init__.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
-from . import click_wrap
 from .interfaces import (
     IPluginPaths,
     ITrayAddon,
-    ITrayModule,
     ITrayAction,
     ITrayService,
     IHostAddon,
@@ -11,32 +9,21 @@ from .interfaces import (
 
 from .base import (
     AYONAddon,
-    OpenPypeModule,
-    OpenPypeAddOn,
-
-    load_modules,
-
-    ModulesManager,
-    TrayModulesManager,
+    AddonsManager,
+    TrayAddonsManager,
+    load_addons,
 )
 
 
 __all__ = (
-    "click_wrap",
-
     "IPluginPaths",
     "ITrayAddon",
-    "ITrayModule",
     "ITrayAction",
     "ITrayService",
     "IHostAddon",
 
     "AYONAddon",
-    "OpenPypeModule",
-    "OpenPypeAddOn",
-
-    "load_modules",
-
-    "ModulesManager",
-    "TrayModulesManager",
+    "AddonsManager",
+    "TrayAddonsManager",
+    "load_addons",
 )
