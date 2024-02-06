f95bb36a1d755756a054c70835cdd995744ed409
Jakub Trllo <jakub.trllo@gmail.com>
2024-02-06 17:56:40 +0100

    add 'ILaunchHookPaths' back

--- client/ayon_core/modules/interfaces.py
+++ client/ayon_core/modules/interfaces.py
@@ -7,6 +7,7 @@ from ayon_core.addon.interfaces import (
 )
 
 ITrayModule = ITrayAddon
+ILaunchHookPaths = object
 
 
 __all__ = (
@@ -16,4 +17,5 @@ __all__ = (
     "ITrayService",
     "IHostAddon",
     "ITrayModule",
+    "ILaunchHookPaths",
 )
