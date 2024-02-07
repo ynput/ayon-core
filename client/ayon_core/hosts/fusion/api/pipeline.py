e981a4106b28eac6bf6c292b07aebb422a9ab2fe
Jakub Trllo <jakub.trllo@gmail.com>
2024-02-07 16:59:22 +0100

    use AYON prefix in fusion env variables

--- client/ayon_core/hosts/fusion/api/pipeline.py
+++ client/ayon_core/hosts/fusion/api/pipeline.py
@@ -308,7 +308,7 @@ class FusionEventThread(QtCore.QThread):
         # getattr of UIManager.GetEvent tries to resolve the Remote Function
         # through the PyRemoteObject
         get_event = app.UIManager.GetEvent
-        delay = int(os.environ.get("OPENPYPE_FUSION_CALLBACK_INTERVAL", 1000))
+        delay = int(os.environ.get("AYON_FUSION_CALLBACK_INTERVAL", 1000))
         while True:
             if self.isInterruptionRequested():
                 return
--- client/ayon_core/hosts/fusion/api/pulse.py
+++ client/ayon_core/hosts/fusion/api/pulse.py
@@ -14,7 +14,7 @@ class PulseThread(QtCore.QThread):
         app = getattr(sys.modules["__main__"], "app", None)
 
         # Interval in milliseconds
-        interval = os.environ.get("OPENPYPE_FUSION_PULSE_INTERVAL", 1000)
+        interval = os.environ.get("AYON_FUSION_PULSE_INTERVAL", 1000)
 
         while True:
             if self.isInterruptionRequested():
