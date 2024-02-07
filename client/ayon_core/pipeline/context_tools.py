2e206476ca47268358dfd0aeaebe5996659e256a
Jakub Trllo <jakub.trllo@gmail.com>
2024-02-07 16:57:22 +0100

    check for 'AYON_REMOTE_PUBLISH'

--- client/ayon_core/pipeline/context_tools.py
+++ client/ayon_core/pipeline/context_tools.py
@@ -146,7 +146,7 @@ def install_host(host):
 
     MessageHandler.emit = modified_emit
 
-    if os.environ.get("OPENPYPE_REMOTE_PUBLISH"):
+    if os.environ.get("AYON_REMOTE_PUBLISH"):
         # target "farm" == rendering on farm, expects OPENPYPE_PUBLISH_DATA
         # target "remote" == remote execution, installs host
         print("Registering pyblish target: remote")
