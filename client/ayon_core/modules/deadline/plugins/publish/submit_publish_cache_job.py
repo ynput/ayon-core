f601728340dcf18eab751067ce310d619dbf81c1
Jakub Trllo <jakub.trllo@gmail.com>
2024-02-07 16:42:45 +0100

    removed unused 'OPENPYPE_METADATA_FILE'

--- client/ayon_core/modules/deadline/plugins/publish/submit_publish_cache_job.py
+++ client/ayon_core/modules/deadline/plugins/publish/submit_publish_cache_job.py
@@ -64,10 +64,6 @@ class ProcessSubmittedCacheJobOnFarm(pyblish.api.InstancePlugin,
 
     families = ["publish.hou"]
 
-    environ_job_filter = [
-        "OPENPYPE_METADATA_FILE"
-    ]
-
     environ_keys = [
         "FTRACK_API_USER",
         "FTRACK_API_KEY",
@@ -146,12 +142,6 @@ class ProcessSubmittedCacheJobOnFarm(pyblish.api.InstancePlugin,
         for env_key in self.environ_keys:
             if os.getenv(env_key):
                 environment[env_key] = os.environ[env_key]
-
-        # pass environment keys from self.environ_job_filter
-        job_environ = job["Props"].get("Env", {})
-        for env_j_key in self.environ_job_filter:
-            if job_environ.get(env_j_key):
-                environment[env_j_key] = job_environ[env_j_key]
 
         priority = self.deadline_priority or instance.data.get("priority", 50)
 
