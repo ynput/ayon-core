ecc5a5d1383b44b8a616b23fa9a4068c19bd9b5f
Jakub Trllo <jakub.trllo@gmail.com>
2024-02-07 16:52:24 +0100

    use 'AYON_OPEN_WORKFILE_POST_INITIALIZATION' instead of 'OPENPYPE_OPEN_WORKFILE_POST_INITIALIZATION'

--- client/ayon_core/hosts/maya/startup/userSetup.py
+++ client/ayon_core/hosts/maya/startup/userSetup.py
@@ -35,7 +35,7 @@ if explicit_plugins_loading["enabled"]:
     )
 
 # Open Workfile Post Initialization.
-key = "OPENPYPE_OPEN_WORKFILE_POST_INITIALIZATION"
+key = "AYON_OPEN_WORKFILE_POST_INITIALIZATION"
 if bool(int(os.environ.get(key, "0"))):
     def _log_and_open():
         path = os.environ["AVALON_LAST_WORKFILE"]
