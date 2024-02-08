00238302088010a68ebcacfbb1f2886727f1c160
Jakub Trllo <jakub.trllo@gmail.com>
2024-02-08 17:00:21 +0100

    use new addon structure with 'package.py'

--- client/ayon_core/version.py
+++ client/ayon_core/version.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-"""Package declaring AYON core version."""
+"""Package declaring AYON core addon version."""
 __version__ = "0.2.0"
--- server/__init__.py
+++ server/__init__.py
@@ -1,13 +1,9 @@
 from ayon_server.addons import BaseServerAddon
 
-from .version import __version__
 from .settings import CoreSettings, DEFAULT_VALUES
 
 
 class CoreAddon(BaseServerAddon):
-    name = "core"
-    title = "Core"
-    version = __version__
     settings_model = CoreSettings
 
     async def get_default_settings(self):
