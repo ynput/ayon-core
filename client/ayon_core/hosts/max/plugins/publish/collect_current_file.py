d4caa4e626fd17c5262491057002950d00a49f24
Kayla Man <moonyuet2008@hotmail.com>
2024-02-14 17:58:10 +0800

    Separating collect current files from the collect workfile

---
+++ client/ayon_core/hosts/max/plugins/publish/collect_current_file.py
@@ -0,0 +1,22 @@
+import os
+import pyblish.api
+
+from pymxs import runtime as rt
+
+
+class CollectCurrentFile(pyblish.api.ContextPlugin):
+    """Inject the current working file."""
+
+    order = pyblish.api.CollectorOrder - 0.4
+    label = "Max Current File"
+    hosts = ['max']
+
+    def process(self, context):
+        """Inject the current working file"""
+        folder = rt.maxFilePath
+        file = rt.maxFileName
+        if not folder or not file:
+            self.log.error("Scene is not saved.")
+        current_file = os.path.join(folder, file)
+
+        context.data["currentFile"] = current_file
--- client/ayon_core/hosts/max/plugins/publish/collect_workfile.py
+++ client/ayon_core/hosts/max/plugins/publish/collect_workfile.py
@@ -12,6 +12,7 @@ class CollectWorkfile(pyblish.api.InstancePlugin):
     order = pyblish.api.CollectorOrder - 0.01
     label = "Collect 3dsmax Workfile"
     hosts = ['max']
+    families = ["workfile"]
 
     def process(self, instance):
         """Inject the current working file."""
@@ -20,33 +21,28 @@ class CollectWorkfile(pyblish.api.InstancePlugin):
         file = rt.maxFileName
         if not folder or not file:
             self.log.error("Scene is not saved.")
-        current_file = os.path.join(folder, file)
-
-        context.data['currentFile'] = current_file
-
         ext = os.path.splitext(file)[-1].lstrip(".")
 
         data = {}
 
-        # create instance
-
         data.update({
-            "setMembers": [current_file],
-            "frameStart": context.data['frameStart'],
-            "frameEnd": context.data['frameEnd'],
-            "handleStart": context.data['handleStart'],
-            "handleEnd": context.data['handleEnd']
+            "setMembers": context.data["currentFile"],
+            "frameStart": context.data["frameStart"],
+            "frameEnd": context.data["frameEnd"],
+            "handleStart": context.data["handleStart"],
+            "handleEnd": context.data["handleEnd"]
         })
 
-        data['representations'] = [{
-            'name': ext,
-            'ext': ext,
-            'files': file,
+        data["representations"] = [{
+            "name": ext,
+            "ext": ext,
+            "files": file,
             "stagingDir": folder,
         }]
 
         instance.data.update(data)
-        self.log.debug('Collected data: {}'.format(data))
-        self.log.debug('Collected instance: {}'.format(file))
-        self.log.debug('Scene path: {}'.format(current_file))
-        self.log.debug('staging Dir: {}'.format(folder))
+        self.log.debug("Collected data: {}".format(data))
+        self.log.debug("Collected instance: {}".format(file))
+        self.log.debug("Scene path: {}".format(
+            context.data["currentFile"]))
+        self.log.debug("staging Dir: {}".format(folder))
