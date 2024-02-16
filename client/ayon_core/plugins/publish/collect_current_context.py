85a2110b385f7855c218b178e94582577cd6adfe
Jakub Trllo <jakub.trllo@gmail.com>
2024-02-16 16:08:52 +0100

    fix used key in collect current context

--- client/ayon_core/plugins/publish/collect_current_context.py
+++ client/ayon_core/plugins/publish/collect_current_context.py
@@ -44,6 +44,6 @@ class CollectCurrentContext(pyblish.api.ContextPlugin):
             "Task: {task_name}"
         ).format(
             project_name=context.data["projectName"],
-            asset_name=context.data["asset"],
+            asset_name=context.data["folderPath"],
             task_name=context.data["task"]
         ))
