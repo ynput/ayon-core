722923f3c09c3433bd0aefede385414fe4d581eb
Jakub Trllo <jakub.trllo@gmail.com>
2024-02-16 09:43:19 +0100

    modified code to use AYON settings

--- client/ayon_core/modules/royalrender/plugins/publish/collect_rr_path_from_instance.py
+++ client/ayon_core/modules/royalrender/plugins/publish/collect_rr_path_from_instance.py
@@ -18,30 +18,21 @@ class CollectRRPathFromInstance(pyblish.api.InstancePlugin):
     def _collect_rr_path_name(instance):
         # type: (pyblish.api.Instance) -> str
         """Get Royal Render pat name from render instance."""
-        rr_settings = (
-            instance.context.data
-            ["system_settings"]
-            ["modules"]
-            ["royalrender"]
-        )
-        if not instance.data.get("rrPaths"):
+
+        instance_rr_paths = instance.data.get("rrPaths")
+        if instance_rr_paths is None:
             return "default"
-        try:
-            default_servers = rr_settings["rr_paths"]
-            project_servers = (
-                instance.context.data
-                ["project_settings"]
-                ["royalrender"]
-                ["rr_paths"]
-            )
-            rr_servers = {
-                k: default_servers[k]
-                for k in project_servers
-                if k in default_servers
-            }
 
-        except (AttributeError, KeyError):
-            # Handle situation were we had only one url for royal render.
-            return rr_settings["rr_paths"]["default"]
+        rr_settings = instance.context.data["project_settings"]["royalrender"]
+        rr_paths = rr_settings["rr_paths"]
+        selected_paths = rr_settings["selected_rr_paths"]
 
-        return list(rr_servers.keys())[int(instance.data.get("rrPaths"))]
+        rr_servers = {
+            path_key
+            for path_key in selected_paths
+            if path_key in rr_paths
+        }
+        for instance_rr_path in instance_rr_paths:
+            if instance_rr_path in rr_servers:
+                return instance_rr_path
+        return "default"
