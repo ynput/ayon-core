05bf9e9ea4da6ef4838018056b6306cc3eeb7715
Jakub Jezek <jakubjezek001@gmail.com>
2024-02-21 22:48:08 +0100

    Refactor workfile creation and instance handling
    - Added method to create workfile instances with updated data
    - Removed redundant file for workfile creation in Nuke host.

--- client/ayon_core/hosts/hiero/plugins/create/create_workfile.py
+++ client/ayon_core/hosts/hiero/plugins/create/create_workfile.py
@@ -1,72 +1,51 @@
-# # -*- coding: utf-8 -*-
-# """Creator plugin for creating workfiles."""
-# from ayon_core.pipeline.create import CreatedInstance, AutoCreator
-# from ayon_core.client import get_asset_by_name
+# -*- coding: utf-8 -*-
+"""Creator plugin for creating workfiles."""
+from ayon_core.pipeline.create import CreatedInstance, AutoCreator
+from ayon_core.client import get_asset_by_name
 
 
-# class CreateWorkfile(AutoCreator):
-#     """Workfile auto-creator."""
-#     identifier = "io.ayon.creators.hiero.workfile"
-#     label = "Workfile"
-#     family = "workfile"
-#     icon = "fa5.file"
+class CreateWorkfile(AutoCreator):
+    """Workfile auto-creator."""
+    identifier = "io.ayon.creators.hiero.workfile"
+    label = "Workfile"
+    family = "workfile"
+    icon = "fa5.file"
 
-#     default_variant = "Main"
+    default_variant = "Main"
 
-#     def create(self):
+    def create(self, options=None):
+        pass
 
-#         variant = self.default_variant
-#         current_instance = next(
-#             (
-#                 instance for instance in self.create_context.instances
-#                 if instance.creator_identifier == self.identifier
-#             ), None)
+    def collect_instances(self):
+        instance_data = {}
+        project_name = self.create_context.get_current_project_name()
+        asset_name = self.create_context.get_current_asset_name()
+        task_name = self.create_context.get_current_task_name()
+        host_name = self.create_context.host_name
 
-#         project_name = self.project_name
-#         asset_name = self.create_context.get_current_asset_name()
-#         task_name = self.create_context.get_current_task_name()
-#         host_name = self.create_context.host_name
+        print(project_name, asset_name, task_name, host_name)
+        asset_doc = get_asset_by_name(project_name, asset_name)
+        subset_name = self.get_subset_name(
+            self.default_variant, task_name, asset_doc,
+            project_name, host_name
+        )
+        instance_data.update({
+            "asset": asset_name,
+            "task": task_name,
+            "variant": self.default_variant
+        })
+        instance_data.update(self.get_dynamic_data(
+            self.default_variant, task_name, asset_doc,
+            project_name, host_name, instance_data
+        ))
 
-#         if current_instance is None:
-#             asset_doc = get_asset_by_name(project_name, asset_name)
-#             subset_name = self.get_subset_name(
-#                 variant, task_name, asset_doc, project_name, host_name
-#             )
-#             data = {
-#                 "asset": asset_name,
-#                 "task": task_name,
-#                 "variant": variant,
-#             }
-#             data.update(
-#                 self.get_dynamic_data(
-#                     variant, task_name, asset_doc,
-#                     project_name, host_name, current_instance)
-#             )
-#             self.log.info("Auto-creating workfile instance...")
-#             current_instance = CreatedInstance(
-#                 self.family, subset_name, data, self
-#             )
-#             self._add_instance_to_context(current_instance)
-#         elif (
-#                 current_instance["asset"] != asset_name
-#                 or current_instance["task"] != task_name
-#         ):
-#             # Update instance context if is not the same
-#             asset_doc = get_asset_by_name(project_name, asset_name)
-#             subset_name = self.get_subset_name(
-#                 variant, task_name, asset_doc, project_name, host_name
-#             )
-#             current_instance["asset"] = asset_name
-#             current_instance["task"] = task_name
-#             current_instance["subset"] = subset_name
+        instance = CreatedInstance(
+            self.family, subset_name, instance_data, self
+        )
+        self._add_instance_to_context(instance)
 
-#     def collect_instances(self):
-#         # TODO: Implement
-#         pass
-
-#     def update_instances(self, update_list):
-#         # TODO: Implement
-#         #   This needs to be implemented to allow persisting any instance
-#         #   data on resets. We'll need to decicde where to store workfile
-#         #   instance data reliably. Likely metadata on the *current project*?
-#         pass
+    def update_instances(self, update_list):
+        # for created_inst, _changes in update_list:
+        #     instance_node = created_inst.transient_data["node"]
+        #     print(instance_node)
+        pass
