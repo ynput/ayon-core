6779d005bc5f1ae2136ee9963c91f29e0fe53ac2
Braden Jennings <braden@ynput.io>
2024-02-22 12:20:04 +1300

    add thumbnails to extract workfile

--- client/ayon_core/hosts/hiero/plugins/publish/extract_workfile.py
+++ client/ayon_core/hosts/hiero/plugins/publish/extract_workfile.py
@@ -5,6 +5,9 @@ from ayon_core.pipeline import publish
 from openpype.hosts.hiero.api import lib
 
 import hiero
+import tempfile
+
+from qtpy.QtGui import QPixmap
 
 
 class ExtractWorkfile(publish.Extractor):
@@ -21,6 +24,37 @@ class ExtractWorkfile(publish.Extractor):
         # create representation data
         if "representations" not in instance.data:
             instance.data["representations"] = []
+
+        # asset = instance.context.data["folderPath"]
+        # asset_name = asset.split("/")[-1]
+
+        active_timeline = hiero.ui.activeSequence()
+        # project = active_timeline.project()
+
+        # adding otio timeline to context
+        # otio_timeline = hiero_export.create_otio_timeline()
+        # otio_timeline = instance.data["otio_timeline"]
+
+        # get workfile thumbnail paths
+        tmp_staging = tempfile.mkdtemp(prefix="pyblish_tmp_")
+        thumbnail_name = "workfile_thumbnail.png"
+        thumbnail_path = os.path.join(tmp_staging, thumbnail_name)
+
+        # search for all windows with name of actual sequence
+        _windows = [w for w in hiero.ui.windowManager().windows()
+                    if active_timeline.name() in w.windowTitle()]
+
+        # export window to thumb path
+        QPixmap.grabWidget(_windows[-1]).save(thumbnail_path, 'png')
+
+        # thumbnail
+        thumb_representation = {
+            'files': thumbnail_name,
+            'stagingDir': tmp_staging,
+            'name': "thumbnail",
+            'thumbnail': True,
+            'ext': "png"
+        }
 
         name = instance.data["name"]
         project = hiero.ui.activeProject()
@@ -43,6 +77,7 @@ class ExtractWorkfile(publish.Extractor):
         }
         representations = instance.data.setdefault("representations", [])
         representations.append(representation)
+        representations.append(thumb_representation)
 
         self.log.debug(
             "Added hiero file representation: {}".format(representation)
