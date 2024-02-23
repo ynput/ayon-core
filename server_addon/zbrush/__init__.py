a5e31ce7443f6de0481350bf47cea7eea1614c62
Kayla Man <moonyuet2008@hotmail.com>
2024-02-23 21:10:45 +0800

    edit __init__.py for zbrush addon

--- server_addon/zbrush/__init__.py
+++ server_addon/zbrush/__init__.py
@@ -3,16 +3,14 @@ from typing import Type
 from ayon_server.addons import BaseServerAddon
 
 from .version import __version__
-from .settings import UnrealSettings, DEFAULT_VALUES
+from .settings import ZbrushSettings, DEFAULT_VALUES
 
 
-class UnrealAddon(BaseServerAddon):
-    name = "unreal"
-    title = "Unreal"
+class ZbrushAddon(BaseServerAddon):
+    name = "zbrush"
+    title = "Zbrush"
     version = __version__
-    settings_model: Type[UnrealSettings] = UnrealSettings
-    frontend_scopes = {}
-    services = {}
+    settings_model: Type[ZbrushSettings] = ZbrushSettings
 
     async def get_default_settings(self):
         settings_model_cls = self.get_settings_model()
