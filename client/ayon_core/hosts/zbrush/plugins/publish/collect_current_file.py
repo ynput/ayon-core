18571bce79afd32bd9c83dc1c3827e8ae84dc557
Roy Nieterau <roy_nieterau@hotmail.com>
2024-03-06 22:19:20 +0100

    Always collect current file to context, even if no active workfile instance

--- client/ayon_core/hosts/zbrush/plugins/publish/collect_current_file.py
+++ client/ayon_core/hosts/zbrush/plugins/publish/collect_current_file.py
@@ -3,17 +3,15 @@ import pyblish.api
 from ayon_core.pipeline import registered_host
 
 
-class CollectCurrentFile(pyblish.api.InstancePlugin):
-    label = "Collect CurrentFile"
+class CollectCurrentFile(pyblish.api.ContextPlugin):
+    label = "Collect Current File"
     order = pyblish.api.CollectorOrder - 0.4
     hosts = ["zbrush"]
-    families = ["workfile"]
 
-    def process(self, instance):
+    def process(self, context):
         host = registered_host()
-        context = instance.context
-        currentFile = host.get_current_workfile()
-        if not currentFile:
+        current_file = host.get_current_workfile()
+        if not current_file:
             self.log.error("Scene is not saved. Please save the "
                            "scene with AYON workfile tools.")
-        context.data["currentFile"] = currentFile
+        context.data["currentFile"] = current_file
