070c3dce3b99cea6197a3e80eceb775f96f43165
Braden Jennings <braden@ynput.io>
2024-03-11 10:52:27 +1300

    clicking in empty area in tree view should switch tab

--- client/ayon_core/tools/publisher/widgets/list_view_widgets.py
+++ client/ayon_core/tools/publisher/widgets/list_view_widgets.py
@@ -317,6 +317,7 @@ class InstanceListGroupWidget(QtWidgets.QFrame):
 class InstanceTreeView(QtWidgets.QTreeView):
     """View showing instances and their groups."""
     toggle_requested = QtCore.Signal(int)
+    double_clicked = QtCore.Signal()
 
     def __init__(self, *args, **kwargs):
         super(InstanceTreeView, self).__init__(*args, **kwargs)
@@ -396,6 +397,7 @@ class InstanceTreeView(QtWidgets.QTreeView):
             super(InstanceTreeView, self).mousePressEvent(event)
 
     def mouseDoubleClickEvent(self, event):
+        self.double_clicked.emit()
         handled = self._mouse_press(event)
         if handled:
             super(InstanceTreeView, self).mouseDoubleClickEvent(event)
@@ -461,7 +463,7 @@ class InstanceListView(AbstractInstanceView):
         instance_view.collapsed.connect(self._on_collapse)
         instance_view.expanded.connect(self._on_expand)
         instance_view.toggle_requested.connect(self._on_toggle_request)
-        instance_view.doubleClicked.connect(self.double_clicked)
+        instance_view.double_clicked.connect(self.double_clicked)
 
         self._group_items = {}
         self._group_widgets = {}
