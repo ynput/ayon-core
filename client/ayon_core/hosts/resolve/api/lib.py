89d2eeaf5000d9c0fdf4b6bfc878b9b971cded60
Jakub Ježek <jakubjezek001@gmail.com>
2024-03-12 10:58:45 +0100

    Update client/ayon_core/hosts/resolve/api/lib.py

--- client/ayon_core/hosts/resolve/api/lib.py
+++ client/ayon_core/hosts/resolve/api/lib.py
@@ -415,9 +415,6 @@ def get_current_timeline_items(
         }
         # get track item object and its color
         for clip_index, ti in enumerate(_clips[track_index]):
-            if not ti.GetMediaPoolItem():
-                continue
-
             data = _data.copy()
             data["clip"] = {
                 "item": ti,
