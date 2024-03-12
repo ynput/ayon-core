29a8a956f386e0571b1030611131bddfcd965b1f
Kayla Man <moonyuet2008@hotmail.com>
2024-03-12 18:12:24 +0800

    move the export format setting into creator setting

--- client/ayon_core/hosts/zbrush/plugins/publish/extract_model.py
+++ client/ayon_core/hosts/zbrush/plugins/publish/extract_model.py
@@ -1,6 +1,5 @@
 import os
 import pyblish.api
-from ayon_core.lib import EnumDef
 from ayon_core.pipeline import publish
 from ayon_core.pipeline.publish import (
     AYONPyblishPluginMixin
@@ -19,16 +18,14 @@ class ExtractModel(publish.Extractor,
     label = "Extract Model"
     hosts = ["zbrush"]
     families = ["model"]
-    export_format = "obj"
 
     def process(self, instance):
         creator_attrs = instance.data["creator_attributes"]
-        attr_values = self.get_attr_values_from_data(instance.data)
-        export_format = attr_values.get("exportFormat")
+        subd_level = creator_attrs.get("subd_level")
+        export_format = creator_attrs.get("exportFormat")
         stagingdir = self.staging_dir(instance)
         filename = f"{instance.name}.{export_format}"
         filepath = os.path.join(stagingdir, filename)
-        subd_level = creator_attrs.get("subd_level")
 
         if "representations" not in instance.data:
             instance.data["representations"] = []
@@ -45,13 +42,3 @@ class ExtractModel(publish.Extractor,
         self.log.info(
             "Extracted instance '%s' to: %s" % (instance.name, filepath)
         )
-
-    @classmethod
-    def get_attribute_defs(cls):
-        export_format_enum = ["abc", "fbx", "ma", "obj"]
-        return[
-            EnumDef("exportFormat",
-                    export_format_enum,
-                    default=cls.export_format,
-                    label="Export Format Options")
-        ]
