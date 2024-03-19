8397e3cc08fe9b88982908621934c45a0d61745b
Tony Dorfmeister <tony.dorfmeister@bepic.studio>
2024-03-19 10:48:02 +0100

    wip: testing options ui

--- client/ayon_core/plugins/load/compare_with_rv.py
+++ client/ayon_core/plugins/load/compare_with_rv.py
@@ -1,4 +1,8 @@
 import json
+import qargparse
+from qtpy import QtCore, QtWidgets, QtGui
+
+
 from ayon_core.lib import ApplicationManager
 from ayon_core.pipeline import load, get_current_context
 
@@ -17,6 +21,22 @@ class CompareWithRV(load.LoaderPlugin):
     icon = "play-circle"
     color = "orange"
     # rvcon = RvCommunicator("ayon")
+
+    options = [
+        qargparse.Choice(
+            "version_to_compare",
+            label="Select version to compare to",
+            items=["1", "2", "3", "4"],
+            default="1",
+            help="Which version to you want to compare to?"
+        ),
+        qargparse.Boolean(
+            "shall_load_plate",
+            label="Load Plate",
+            default=False,
+            help="Loads the main plate aswell"
+        ),
+    ]
 
     def load(self, context, name, namespace, data):
         self.log.warning(f"{self.rvcon.connected = }")
