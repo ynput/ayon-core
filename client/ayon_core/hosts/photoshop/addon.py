2dc170a27d8ab48febf51a8e71c9965bee5f5225
Jakub Trllo <jakub.trllo@gmail.com>
2024-03-25 15:53:17 +0100

    photoshop has own prelaunch hook to modify launch arguments

--- client/ayon_core/hosts/photoshop/addon.py
+++ client/ayon_core/hosts/photoshop/addon.py
@@ -20,3 +20,17 @@ class PhotoshopAddon(AYONAddon, IHostAddon):
 
     def get_workfile_extensions(self):
         return [".psd", ".psb"]
+
+    def get_launch_hook_paths(self, app):
+        if app.host_name != self.host_name:
+            return []
+        return [
+            os.path.join(PHOTOSHOP_ADDON_ROOT, "hooks")
+        ]
+
+
+def get_launch_script_path():
+    return os.path.join(
+        PHOTOSHOP_ADDON_ROOT, "api", "launch_script.py"
+    )
+
