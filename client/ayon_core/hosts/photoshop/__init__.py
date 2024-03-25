aaea1e14859799dd8c443c832dce2ffa75be8e3f
Jakub Trllo <jakub.trllo@gmail.com>
2024-03-25 15:53:17 +0100

    photoshop has own prelaunch hook to modify launch arguments

--- client/ayon_core/hosts/photoshop/__init__.py
+++ client/ayon_core/hosts/photoshop/__init__.py
@@ -1,10 +1,12 @@
 from .addon import (
     PHOTOSHOP_ADDON_ROOT,
     PhotoshopAddon,
+    get_launch_script_path,
 )
 
 
 __all__ = (
     "PHOTOSHOP_ADDON_ROOT",
     "PhotoshopAddon",
+    "get_launch_script_path",
 )
