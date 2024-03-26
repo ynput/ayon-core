587df9ca2d971a02dff457070dbc3fb103dfe329
Petr Kalis <petr.kalis@gmail.com>
2024-03-26 15:34:40 +0100

    AY-745 - renamed class

--- server_addon/deadline/server/settings/site_settings.py
+++ server_addon/deadline/server/settings/site_settings.py
@@ -5,7 +5,7 @@ from ayon_server.settings import (
 from .main import defined_deadline_ws_name_enum_resolver
 
 
-class LocalSubmodel(BaseSettingsModel):
+class CredentialPerServerModel(BaseSettingsModel):
     """Provide credentials for configured DL servers"""
     _layout = "expanded"
     server_name: str = SettingsField("",
@@ -18,7 +18,7 @@ class LocalSubmodel(BaseSettingsModel):
 
 
 class DeadlineSiteSettings(BaseSettingsModel):
-    local_settings: list[LocalSubmodel] = SettingsField(
+    local_settings: list[CredentialPerServerModel] = SettingsField(
         default_factory=list,
         title="Local setting",
         description="Please provide credentials for configured Deadline servers",
