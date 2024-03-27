29f9acbd79d718dba685c6bedf531a5211f01bef
Roy Nieterau <roy_nieterau@hotmail.com>
2024-03-27 14:47:31 +0100

    Maya: Yeti - Implement writing and loading user variables with a yeti cache

--- client/ayon_core/hosts/maya/plugins/load/load_yeti_cache.py
+++ client/ayon_core/hosts/maya/plugins/load/load_yeti_cache.py
@@ -12,6 +12,7 @@ from ayon_core.pipeline import (
     get_representation_path
 )
 from ayon_core.hosts.maya.api import lib
+from ayon_core.hosts.maya.api.yeti import create_yeti_variable
 from ayon_core.hosts.maya.api.pipeline import containerise
 from ayon_core.hosts.maya.api.plugin import get_load_color_for_product_type
 
@@ -23,7 +24,18 @@ SKIP_UPDATE_ATTRS = {
     "viewportDensity",
     "viewportWidth",
     "viewportLength",
+    "renderDensity",
+    "renderWidth",
+    "renderLength",
+    "increaseRenderBounds"
 }
+
+SKIP_ATTR_MESSAGE = (
+    "Skipping updating %s.%s to %s because it "
+    "is considered a local overridable attribute. "
+    "Either set manually or the load the cache "
+    "anew."
+)
 
 
 def set_attribute(node, attr, value):
@@ -209,8 +221,30 @@ class YetiCacheLoader(load.LoaderPlugin):
 
                     for attr, value in node_settings["attrs"].items():
                         if attr in SKIP_UPDATE_ATTRS:
+                            self.log.info(
+                                SKIP_ATTR_MESSAGE, yeti_node, attr, value
+                            )
                             continue
                         set_attribute(attr, value, yeti_node)
+
+                    # Set up user defined attributes
+                    user_variables = node_settings.get("user_variables", {})
+                    for attr, value in user_variables.items():
+                        was_value_set = create_yeti_variable(
+                            yeti_shape_node=yeti_node,
+                            attr_name=attr,
+                            value=value,
+                            # We do not want to update the
+                            # value if it already exists so
+                            # that any local overrides that
+                            # may have been applied still
+                            # persist
+                            force_value=False
+                        )
+                        if not was_value_set:
+                            self.log.info(
+                                SKIP_ATTR_MESSAGE, yeti_node, attr, value
+                            )
 
         cmds.setAttr("{}.representation".format(container_node),
                      repre_entity["id"],
@@ -331,6 +365,13 @@ class YetiCacheLoader(load.LoaderPlugin):
         # Apply attributes to pgYetiMaya node
         for attr, value in attributes.items():
             set_attribute(attr, value, yeti_node)
+
+        # Set up user defined attributes
+        user_variables = node_settings.get("user_variables", {})
+        for attr, value in user_variables.items():
+            create_yeti_variable(yeti_shape_node=yeti_node,
+                                 attr_name=attr,
+                                 value=value)
 
         # Connect to the time node
         cmds.connectAttr("time1.outTime", "%s.currentTime" % yeti_node)
