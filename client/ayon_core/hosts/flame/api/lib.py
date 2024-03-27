005271b28629e6cf5abe2c7c785661c2bd12beb4
Jakub Trllo <jakub.trllo@gmail.com>
2024-03-27 12:26:32 +0100

    renamed 'get_reformated_filename' to 'get_reformatted_filename'

--- client/ayon_core/hosts/flame/api/lib.py
+++ client/ayon_core/hosts/flame/api/lib.py
@@ -607,7 +607,7 @@ def get_clips_in_reels(project):
     return output_clips
 
 
-def get_reformated_filename(filename, padded=True):
+def get_reformatted_filename(filename, padded=True):
     """
     Return fixed python expression path
 
@@ -618,7 +618,7 @@ def get_reformated_filename(filename, padded=True):
         type: string with reformatted path
 
     Example:
-        get_reformated_filename("plate.1001.exr") > plate.%04d.exr
+        get_reformatted_filename("plate.1001.exr") > plate.%04d.exr
 
     """
     found = FRAME_PATTERN.search(filename)
