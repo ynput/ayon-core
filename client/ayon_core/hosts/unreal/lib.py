18ac3b4a44c299cc714f7b59388e50eab28dda0f
Jakub Trllo <jakub.trllo@gmail.com>
2024-03-27 12:34:47 +0100

    remove unnecessary f-strings in unreal lib

--- client/ayon_core/hosts/unreal/lib.py
+++ client/ayon_core/hosts/unreal/lib.py
@@ -236,10 +236,12 @@ def create_unreal_project(project_name: str,
     project_file = pr_dir / f"{unreal_project_name}.uproject"
 
     print("--- Generating a new project ...")
-    commandlet_cmd = [f'{ue_editor_exe.as_posix()}',
-                      f'{cmdlet_project.as_posix()}',
-                      f'-run=AyonGenerateProject',
-                      f'{project_file.resolve().as_posix()}']
+    commandlet_cmd = [
+        ue_editor_exe.as_posix(),
+        cmdlet_project.as_posix(),
+        "-run=AyonGenerateProject",
+        project_file.resolve().as_posix()
+    ]
 
     if dev_mode or preset["dev_mode"]:
         commandlet_cmd.append('-GenerateCode')
@@ -266,7 +268,7 @@ def create_unreal_project(project_name: str,
         pf.seek(0)
         json.dump(pf_json, pf, indent=4)
         pf.truncate()
-        print(f'--- Engine ID has been written into the project file')
+        print("--- Engine ID has been written into the project file")
 
     if dev_mode or preset["dev_mode"]:
         u_build_tool = get_path_to_ubt(engine_path, ue_version)
@@ -280,17 +282,25 @@ def create_unreal_project(project_name: str,
             # we need to test this out
             arch = "Mac"
 
-        command1 = [u_build_tool.as_posix(), "-projectfiles",
-                    f"-project={project_file}", "-progress"]
+        command1 = [
+            u_build_tool.as_posix(),
+            "-projectfiles",
+            f"-project={project_file}",
+            "-progress"
+        ]
 
         subprocess.run(command1)
 
-        command2 = [u_build_tool.as_posix(),
-                    f"-ModuleWithSuffix={unreal_project_name},3555", arch,
-                    "Development", "-TargetType=Editor",
-                    f'-Project={project_file}',
-                    f'{project_file}',
-                    "-IgnoreJunk"]
+        command2 = [
+            u_build_tool.as_posix(),
+            f"-ModuleWithSuffix={unreal_project_name},3555",
+            arch,
+            "Development",
+            "-TargetType=Editor",
+            f"-Project={project_file}",
+            project_file,
+            "-IgnoreJunk"
+        ]
 
         subprocess.run(command2)
 
