09b251494784f45e8f6397add2b5e44c132d8c9f
Roy Nieterau <roy_nieterau@hotmail.com>
2024-03-28 14:00:05 +0100

    Add description to report

--- client/ayon_core/hosts/maya/plugins/publish/validate_current_renderlayer_renderable.py
+++ client/ayon_core/hosts/maya/plugins/publish/validate_current_renderlayer_renderable.py
@@ -1,3 +1,5 @@
+import inspect
+
 import pyblish.api
 
 from maya import cmds
@@ -10,7 +12,7 @@ from ayon_core.pipeline.publish import (
 
 class ValidateCurrentRenderLayerIsRenderable(pyblish.api.ContextPlugin,
                                              OptionalPyblishPluginMixin):
-    """Validate if current render layer has a renderable camera
+    """Validate if current render layer has a renderable camera.
 
     There is a bug in Redshift which occurs when the current render layer
     at file open has no renderable camera. The error raised is as follows:
@@ -41,5 +43,6 @@ class ValidateCurrentRenderLayerIsRenderable(pyblish.api.ContextPlugin,
             raise PublishValidationError(
                 "Current render layer '{}' has no renderable camera".format(
                     layer
-                )
+                ),
+                description=inspect.getdoc(self)
             )
