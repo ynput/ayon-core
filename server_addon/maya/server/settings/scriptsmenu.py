dd77f243ddd2eaf3ed26ca8ec66d50af6e914451
Roy Nieterau <roy_nieterau@hotmail.com>
2024-04-03 11:42:10 +0200

    Allow specifying raw JSON custom tools menu for Maya.

--- server_addon/maya/server/settings/scriptsmenu.py
+++ server_addon/maya/server/settings/scriptsmenu.py
@@ -1,3 +1,7 @@
+import json
+
+from pydantic import validator
+from ayon_server.exceptions import BadRequestException
 from ayon_server.settings import BaseSettingsModel, SettingsField
 
 
@@ -15,18 +19,52 @@ class ScriptsmenuSubmodel(BaseSettingsModel):
 
 
 class ScriptsmenuModel(BaseSettingsModel):
+    """Add a custom scripts menu to Maya"""
     _isGroup = True
 
     name: str = SettingsField(title="Menu Name")
+    use_json_definition: bool = SettingsField(
+        title="Use Raw JSON Definition",
+        description="When enabled, the definition field will be ignored. "
+                    "Instead the menu will be build from the raw JSON "
+                    "definition below it."
+    )
     definition: list[ScriptsmenuSubmodel] = SettingsField(
         default_factory=list,
         title="Menu Definition",
         description="Scriptmenu Items Definition"
     )
 
+    definition_json: str = SettingsField(
+        "[]", title="Definition JSON", widget="textarea",
+        description=(
+            "When Use Raw JSON definition is enabled this field will be used "
+            "to put the direct JSON content instead of using the definition "
+            "menu builder UI above. For more details on the JSON format, see "
+            "[here](https://github.com/Colorbleed/scriptsmenu?tab=readme-ov-file#configuration)."  # noqa: E501
+        )
+    )
+
+    @validator("definition_json")
+    def validate_json(cls, value):
+        if not value.strip():
+            return "[]"
+        try:
+            converted_value = json.loads(value)
+            success = isinstance(converted_value, list)
+        except json.JSONDecodeError:
+            success = False
+
+        if not success:
+            raise BadRequestException(
+                "The definition can't be parsed as json list object"
+            )
+        return value
+
 
 DEFAULT_SCRIPTSMENU_SETTINGS = {
     "name": "Custom Tools",
+    "use_json_definition": False,
     "definition": [
         {
             "type": "action",
@@ -39,5 +77,6 @@ DEFAULT_SCRIPTSMENU_SETTINGS = {
                 "shader"
             ]
         }
-    ]
+    ],
+    "definition_raw": ""
 }
