f4b1c28b8618ba4f26488ff66793317e88ee466e
Roy Nieterau <roy_nieterau@hotmail.com>
2024-04-03 16:16:17 +0200

    Update client/ayon_core/pipeline/workfile/workfile_template_builder.py
    Co-authored-by: Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>

--- client/ayon_core/pipeline/workfile/workfile_template_builder.py
+++ client/ayon_core/pipeline/workfile/workfile_template_builder.py
@@ -498,9 +498,14 @@ class AbstractTemplateBuilder(object):
                                               process if version is created
 
         """
-        if any(value is None for value in [template_path,
-                                           keep_placeholders,
-                                           create_first_version]):
+        if any(
+            value is None
+            for value in [
+                template_path,
+                keep_placeholders,
+                create_first_version,
+            ]
+        ):
             template_preset = self.get_template_preset()
             if template_path is None:
                 template_path = template_preset["path"]
