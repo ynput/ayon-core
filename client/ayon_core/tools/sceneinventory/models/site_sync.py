ccc3c1710acda86eeaf4840a88384f127c55f549
Jakub Trllo <jakub.trllo@gmail.com>
2024-04-04 11:01:48 +0200

    use 'sitesync' over 'sync_server'

--- client/ayon_core/tools/sceneinventory/models/site_sync.py
+++ client/ayon_core/tools/sceneinventory/models/site_sync.py
@@ -9,30 +9,30 @@ class SiteSyncModel:
     def __init__(self, controller):
         self._controller = controller
 
-        self._sync_server_module = NOT_SET
-        self._sync_server_enabled = None
+        self._sitesync_addon = NOT_SET
+        self._sitesync_enabled = None
         self._active_site = NOT_SET
         self._remote_site = NOT_SET
         self._active_site_provider = NOT_SET
         self._remote_site_provider = NOT_SET
 
     def reset(self):
-        self._sync_server_module = NOT_SET
-        self._sync_server_enabled = None
+        self._sitesync_addon = NOT_SET
+        self._sitesync_enabled = None
         self._active_site = NOT_SET
         self._remote_site = NOT_SET
         self._active_site_provider = NOT_SET
         self._remote_site_provider = NOT_SET
 
-    def is_sync_server_enabled(self):
+    def is_sitesync_enabled(self):
         """Site sync is enabled.
 
         Returns:
             bool: Is enabled or not.
         """
 
-        self._cache_sync_server_module()
-        return self._sync_server_enabled
+        self._cache_sitesync_addon()
+        return self._sitesync_enabled
 
     def get_site_provider_icons(self):
         """Icon paths per provider.
@@ -41,9 +41,9 @@ class SiteSyncModel:
             dict[str, str]: Path by provider name.
         """
 
-        if not self.is_sync_server_enabled():
+        if not self.is_sitesync_enabled():
             return {}
-        site_sync_addon = self._get_sync_server_module()
+        site_sync_addon = self._get_sitesync_addon()
         return site_sync_addon.get_site_icons()
 
     def get_sites_information(self):
@@ -65,11 +65,11 @@ class SiteSyncModel:
             }
             for repre_id in representation_ids
         }
-        if not self.is_sync_server_enabled():
+        if not self.is_sitesync_enabled():
             return output
 
         project_name = self._controller.get_current_project_name()
-        site_sync = self._get_sync_server_module()
+        site_sync = self._get_sitesync_addon()
         repre_entities = ayon_api.get_representations(
             project_name, representation_ids
         )
@@ -95,7 +95,7 @@ class SiteSyncModel:
         """
 
         project_name = self._controller.get_current_project_name()
-        site_sync = self._get_sync_server_module()
+        site_sync = self._get_sitesync_addon()
         active_site = self._get_active_site()
         remote_site = self._get_remote_site()
         progress = self.get_representations_site_progress(
@@ -119,18 +119,18 @@ class SiteSyncModel:
                     project_name, repre_id, site, force=True
                 )
 
-    def _get_sync_server_module(self):
-        self._cache_sync_server_module()
-        return self._sync_server_module
+    def _get_sitesync_addon(self):
+        self._cache_sitesync_addon()
+        return self._sitesync_addon
 
-    def _cache_sync_server_module(self):
-        if self._sync_server_module is not NOT_SET:
-            return self._sync_server_module
+    def _cache_sitesync_addon(self):
+        if self._sitesync_addon is not NOT_SET:
+            return self._sitesync_addon
         manager = AddonsManager()
-        site_sync = manager.get("sync_server")
+        site_sync = manager.get("sitesync")
         sync_enabled = site_sync is not None and site_sync.enabled
-        self._sync_server_module = site_sync
-        self._sync_server_enabled = sync_enabled
+        self._sitesync_addon = site_sync
+        self._sitesync_enabled = sync_enabled
 
     def _get_active_site(self):
         if self._active_site is NOT_SET:
@@ -157,8 +157,8 @@ class SiteSyncModel:
         remote_site = None
         active_site_provider = None
         remote_site_provider = None
-        if self.is_sync_server_enabled():
-            site_sync = self._get_sync_server_module()
+        if self.is_sitesync_enabled():
+            site_sync = self._get_sitesync_addon()
             project_name = self._controller.get_current_project_name()
             active_site = site_sync.get_active_site(project_name)
             remote_site = site_sync.get_remote_site(project_name)
