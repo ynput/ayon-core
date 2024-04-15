bcafa9e2d8aa8de0e140952ef598432d684895fa
MustafaJafar <mustafataherzaky@outlook.com>
2024-04-15 17:03:00 +0200

    implement update_node_parameters and refactor CreateRedshiftROP a little bit

--- client/ayon_core/hosts/houdini/plugins/create/create_redshift_rop.py
+++ client/ayon_core/hosts/houdini/plugins/create/create_redshift_rop.py
@@ -4,7 +4,7 @@ import hou  # noqa
 
 from ayon_core.pipeline import CreatorError
 from ayon_core.hosts.houdini.api import plugin
-from ayon_core.lib import EnumDef, BoolDef
+from ayon_core.lib import EnumDef
 
 
 class CreateRedshiftROP(plugin.HoudiniCreator):
@@ -56,24 +56,24 @@ class CreateRedshiftROP(plugin.HoudiniCreator):
 
         # Set the linked rop to the Redshift ROP
         ipr_rop.parm("linked_rop").set(instance_node.path())
-        ext = pre_create_data.get("image_format")
-        multi_layered_mode = pre_create_data.get("multi_layered_mode")
-
-        ext_format_index = {"exr": 0, "tif": 1, "jpg": 2, "png": 3}
-        multilayer_mode_index = {"No Multi-Layered EXR File": "1",
-                                 "Full Multi-Layered EXR File": "2" }
 
         filepath = "{renders_dir}{product_name}/{product_name}.{fmt}".format(
                 renders_dir=hou.text.expandString("$HIP/pyblish/renders/"),
                 product_name=product_name,
-                fmt="$AOV.$F4.{ext}".format(ext=ext)
+                fmt="$AOV.$F4.{ext}".format(ext=pre_create_data.get("image_format"))
             )
 
-        if multilayer_mode_index[multi_layered_mode] == "1":
-            multipart = False
+        rs_filepath = "{export_dir}{product_name}/{product_name}.$F4.rs".format(
+            export_dir=hou.text.expandString("$HIP/pyblish/rs/"),
+            product_name=product_name
+        )
 
-        elif multilayer_mode_index[multi_layered_mode] == "2":
-            multipart = True
+        camera = ""
+        if self.selected_nodes:
+            # set up the render camera from the selected node
+            for node in self.selected_nodes:
+                if node.type().name() == "cam":
+                    camera = node.path()
 
         parms = {
             # Render frame range
@@ -81,28 +81,14 @@ class CreateRedshiftROP(plugin.HoudiniCreator):
             # Redshift ROP settings
             "RS_outputFileNamePrefix": filepath,
             "RS_outputBeautyAOVSuffix": "beauty",
-            "RS_outputFileFormat": ext_format_index[ext],
+            "RS_archive_file": rs_filepath,
+            "RS_renderCamera": camera
         }
-        if ext == "exr":
-            parms["RS_outputMultilayerMode"] = multilayer_mode_index[multi_layered_mode]
-            parms["RS_aovMultipart"] = multipart
-
-        if self.selected_nodes:
-            # set up the render camera from the selected node
-            camera = None
-            for node in self.selected_nodes:
-                if node.type().name() == "cam":
-                    camera = node.path()
-            parms["RS_renderCamera"] = camera or ""
-
-        export_dir = hou.text.expandString("$HIP/pyblish/rs/")
-        rs_filepath = f"{export_dir}{product_name}/{product_name}.$F4.rs"
-        parms["RS_archive_file"] = rs_filepath
-
-        if pre_create_data.get("split_render", self.split_render):
-            parms["RS_archive_enable"] = 1
 
         instance_node.setParms(parms)
+
+        # Set parameters that depends on the value of creator data.
+        self.update_node_parameters(instance_node, pre_create_data)
 
         # Lock some Avalon attributes
         to_lock = ["productType", "id"]
@@ -118,30 +104,62 @@ class CreateRedshiftROP(plugin.HoudiniCreator):
 
         return super(CreateRedshiftROP, self).remove_instances(instances)
 
-    def get_pre_create_attr_defs(self):
-        attrs = super(CreateRedshiftROP, self).get_pre_create_attr_defs()
+    @staticmethod
+    def update_node_parameters(node, creator_attributes):
+        """update node parameters according to creator attributes.
+
+        Implementation of update_node_parameters.
+
+        Args:
+            node(hou.Node): Houdini node to apply changes to.
+            creator_attributes(dict): Dictionary of creator attributes.
+        """
+
+        ext = creator_attributes.get("image_format")
+        ext_format_index = {"exr": 0, "tif": 1, "jpg": 2, "png": 3}
+
+        parms = {"RS_outputFileFormat": ext_format_index[ext]}
+
+        multi_layered_mode = creator_attributes.get("multi_layered_mode")
+        multipart = False
+        if multi_layered_mode == "2":
+            multipart = True
+
+        if ext == "exr":
+            parms["RS_outputMultilayerMode"] = multi_layered_mode
+            parms["RS_aovMultipart"] = multipart
+
+        if creator_attributes.get("render_target") == "farm_split":
+            parms["RS_archive_enable"] = 1
+
+        node.setParms(parms)
+
+    def get_instance_attr_defs(self):
         image_format_enum = [
             "exr", "tif", "jpg", "png",
         ]
-        multi_layered_mode = [
-            "No Multi-Layered EXR File",
-            "Full Multi-Layered EXR File"
-        ]
+        multi_layered_mode = {
+            "1": "No Multi-Layered EXR File",
+            "2": "Full Multi-Layered EXR File"
+        }
+        render_target_items = {
+            "local": "Local machine rendering",
+            "local_no_render": "Use existing frames (local)",
+            "farm": "Farm Rendering",
+            "farm_split": "Farm Rendering - Split export & render jobs",
+        }
 
-
-        return attrs + [
-            BoolDef("farm",
-                    label="Submitting to Farm",
-                    default=True),
-            BoolDef("split_render",
-                    label="Split export and render jobs",
-                    default=self.split_render),
+        return [
+            EnumDef("render_target",
+                    items=render_target_items,
+                    label="Render target",
+                    default=self.render_target),
             EnumDef("image_format",
                     image_format_enum,
                     default=self.ext,
                     label="Image Format Options"),
             EnumDef("multi_layered_mode",
-                    multi_layered_mode,
+                    items=multi_layered_mode,
                     default=self.multi_layered_mode,
                     label="Multi-Layered EXR")
         ]
