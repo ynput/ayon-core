d37c471a81cfb9d9ab6e9dd95285bcec9e867f74
Kayla Man <moonyuet2008@hotmail.com>
2024-04-15 22:09:22 +0800

    add boolean options for the frame range validator for operator isntance

--- client/ayon_core/hosts/max/plugins/publish/collect_tycache_attributes.py
+++ client/ayon_core/hosts/max/plugins/publish/collect_tycache_attributes.py
@@ -1,9 +1,12 @@
 import pyblish.api
 import copy
+from ayon_core.lib import BoolDef
+from ayon_core.pipeline.publish import AYONPyblishPluginMixin
 from pymxs import runtime as rt
 
 
-class CollectTyFlowData(pyblish.api.InstancePlugin):
+class CollectTyFlowData(pyblish.api.InstancePlugin,
+                        AYONPyblishPluginMixin):
     """Collect Channel Attributes for TyCache Export"""
 
     order = pyblish.api.CollectorOrder + 0.005
@@ -18,6 +21,7 @@ class CollectTyFlowData(pyblish.api.InstancePlugin):
                 name for name
                 in container.modifiers[0].AYONTyCacheData.tyc_exports
         ]
+        attr_values = self.get_attr_values_from_data(instance.data)
         # TODO: need to do regex when the export particle has some names without regex.
         for tyc_product_name in tyc_product_names:
             self.log.debug(f"Creating instance for operator:{tyc_product_name}")
@@ -26,10 +30,20 @@ class CollectTyFlowData(pyblish.api.InstancePlugin):
             tyc_instance.data.update(copy.deepcopy(dict(instance.data)))
             tyc_instance.data["name"] = tyc_product_name
             tyc_instance.data["label"] = tyc_product_name
+            tyc_instance.data["family"] = instance.data["tyc_exportMode"]
             tyc_instance.data["productName"] = tyc_product_name
             tyc_instance.data["productType"] = instance.data["tyc_exportMode"]
             tyc_instance.data["exportMode"] = (
                 2 if instance.data["tyc_exportMode"] == "tycache" else 6
             )
             tyc_instance.data["families"] = [instance.data["tyc_exportMode"]]
+            tyc_instance.data["publish_attributes"] = {"ValidateTyCacheFrameRange":{"active": True}}
             instance.append(tyc_instance)
+
+    @classmethod
+    def get_attribute_defs(cls):
+        return [
+            BoolDef("has_frame_range_validator",
+                    label="Validate TyCache Frame Range",
+                    default=True),
+        ]
