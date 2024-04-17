03cfed2c7957538a9c72afe4e31c010cb1d606ab
Roy Nieterau <roy_nieterau@hotmail.com>
2024-04-17 20:33:39 +0200

    Add more family so attribute definitions show in Nuke and Fusion for `render`, `image` and `prerender`.
    This should be safe because `instance.data.get("farm")` is checked in `process()` and if not true the processing is skipped anyway - so if e.g. a render instance in Fusion is set to render local instead of on the farm the actual attribute definition does show - but the processing of the plug-in is skipped regardless.

--- client/ayon_core/modules/deadline/plugins/publish/submit_publish_job.py
+++ client/ayon_core/modules/deadline/plugins/publish/submit_publish_job.py
@@ -88,9 +88,9 @@ class ProcessSubmittedJobOnFarm(pyblish.api.InstancePlugin,
     hosts = ["fusion", "max", "maya", "nuke", "houdini",
              "celaction", "aftereffects", "harmony", "blender"]
 
-    families = ["render.farm", "render.frames_farm",
-                "prerender.farm", "prerender.frames_farm",
-                "renderlayer", "imagesequence",
+    families = ["render", "render.farm", "render.frames_farm",
+                "prerender", "prerender.farm", "prerender.frames_farm",
+                "renderlayer", "imagesequence", "image",
                 "vrayscene", "maxrender",
                 "arnold_rop", "mantra_rop",
                 "karma_rop", "vray_rop",
@@ -310,7 +310,6 @@ class ProcessSubmittedJobOnFarm(pyblish.api.InstancePlugin,
         deadline_publish_job_id = response.json()["_id"]
 
         return deadline_publish_job_id
-
 
     def process(self, instance):
         # type: (pyblish.api.Instance) -> None
