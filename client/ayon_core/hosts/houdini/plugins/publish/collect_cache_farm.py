311e9a3304a9f6203283c8a95e75fbb7d0816800
MustafaJafar <mustafataherzaky@outlook.com>
2024-04-17 11:17:24 +0200

    support $F in Houdini pointcache Abc product type

--- client/ayon_core/hosts/houdini/plugins/publish/collect_cache_farm.py
+++ client/ayon_core/hosts/houdini/plugins/publish/collect_cache_farm.py
@@ -7,7 +7,7 @@ from ayon_core.hosts.houdini.api import lib
 class CollectDataforCache(pyblish.api.InstancePlugin):
     """Collect data for caching to Deadline."""
 
-    order = pyblish.api.CollectorOrder + 0.04
+    order = pyblish.api.CollectorOrder + 0.11
     families = ["ass", "pointcache",
                 "mantraifd", "redshiftproxy",
                 "vdbcache"]
