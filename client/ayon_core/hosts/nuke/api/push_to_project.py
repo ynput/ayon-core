2082214b356671d9a28854ad7ee2a32e2509d1db
Toke Stuart Jepsen <tokejepsen@gmail.com>
2024-05-07 21:11:29 +0100

    BigRoy feedback

--- client/ayon_core/hosts/nuke/api/push_to_project.py
+++ client/ayon_core/hosts/nuke/api/push_to_project.py
@@ -7,7 +7,7 @@ from ayon_core.settings import get_project_settings
 from ayon_core.pipeline import Anatomy, registered_host
 from ayon_core.pipeline.template_data import get_template_data
 from ayon_core.pipeline.workfile import get_workdir_with_workdir_data
-from ayon_core.tools.context_dialog import show
+from ayon_core.tools import context_dialog
 
 from .utils import bake_gizmos_recursively
 from .lib import MENU_LABEL
@@ -46,7 +46,7 @@ def bake_container(container):
 
 
 def main():
-    context = show()
+    context = context_dialog.show()
 
     if context is None:
         return
