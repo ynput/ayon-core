475d1db69bc518522b57f270f174799842f6d031
MustafaJafar <mustafataherzaky@outlook.com>
2024-05-09 15:05:54 +0300

    add 'model' product type to 'collect_instnaces_type'

--- client/ayon_core/hosts/houdini/plugins/publish/collect_instances_type.py
+++ client/ayon_core/hosts/houdini/plugins/publish/collect_instances_type.py
@@ -1,21 +1,24 @@
-"""Collector for pointcache types.
+"""Collector for different types.
 
-This will add additional family to pointcache instance based on
+This will add additional families to different instance based on
 the creator_identifier parameter.
 """
 import pyblish.api
 
 
 class CollectPointcacheType(pyblish.api.InstancePlugin):
-    """Collect data type for pointcache instance."""
+    """Collect data type for different instances."""
 
     order = pyblish.api.CollectorOrder
     hosts = ["houdini"]
-    families = ["pointcache"]
-    label = "Collect type of pointcache"
+    families = ["pointcache", "model"]
+    label = "Collect instances types"
 
     def process(self, instance):
         if instance.data["creator_identifier"] == "io.openpype.creators.houdini.bgeo":  # noqa: E501
             instance.data["families"] += ["bgeo"]
-        elif instance.data["creator_identifier"] == "io.openpype.creators.houdini.pointcache":  # noqa: E501
+        elif instance.data["creator_identifier"] in {
+            "io.openpype.creators.houdini.pointcache",
+            "io.openpype.creators.houdini.model"
+        }:
             instance.data["families"] += ["abc"]
