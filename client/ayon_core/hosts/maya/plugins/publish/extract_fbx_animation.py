839c6432fda640608a5ea8d22f5ca7eb796debd7
Kayla Man <moonyuet2008@hotmail.com>
2024-05-11 00:00:52 +0800

    add joint into accepted_controllers & make sure the bake animation doesn't get resample when it is exported

--- client/ayon_core/hosts/maya/plugins/publish/extract_fbx_animation.py
+++ client/ayon_core/hosts/maya/plugins/publish/extract_fbx_animation.py
@@ -36,6 +36,7 @@ class ExtractFBXAnimation(publish.Extractor):
         out_members = instance.data.get("animated_skeleton", [])
         # Export
         instance.data["constraints"] = True
+        instance.data["bakeResampleAnimation"] = False
         instance.data["skeletonDefinitions"] = True
         instance.data["referencedAssetsContent"] = True
         fbx_exporter.set_options_from_instance(instance)
