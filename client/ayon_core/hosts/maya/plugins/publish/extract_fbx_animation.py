0cd1bdf434cb5cacc29853c141238b938c2dc71c
Kayla Man <moonyuet2008@hotmail.com>
2024-05-13 18:56:56 +0800

    make sure the constraint is false when exporting

--- client/ayon_core/hosts/maya/plugins/publish/extract_fbx_animation.py
+++ client/ayon_core/hosts/maya/plugins/publish/extract_fbx_animation.py
@@ -35,8 +35,7 @@ class ExtractFBXAnimation(publish.Extractor):
         fbx_exporter = fbx.FBXExtractor(log=self.log)
         out_members = instance.data.get("animated_skeleton", [])
         # Export
-        instance.data["constraints"] = True
-        instance.data["bakeResampleAnimation"] = False
+        instance.data["constraints"] = False
         instance.data["skeletonDefinitions"] = True
         instance.data["referencedAssetsContent"] = True
         fbx_exporter.set_options_from_instance(instance)
