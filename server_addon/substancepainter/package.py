50a3b7bda207caf36c3375455d0ad0b8ca46ad9f
Kayla Man <moonyuet2008@hotmail.com>
2024-05-15 16:06:38 +0800

    move substance painter integration to server-addon and rename the folder as ayon_substancepainter

--- server_addon/substancepainter/package.py
+++ server_addon/substancepainter/package.py
@@ -1,3 +1,10 @@
 name = "substancepainter"
 title = "Substance Painter"
-version = "0.1.1"
+version = "0.1.2"
+
+client_dir = "ayon_substancepainter"
+
+ayon_required_addons = {
+    "core": ">=0.3.1",
+}
+ayon_compatible_addons = {}
