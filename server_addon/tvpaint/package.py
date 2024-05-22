2862bb3eb311148a6ac051e566b098a7bcbf1d99
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-05-22 17:59:35 +0200

    change version and add more information about addon

--- server_addon/tvpaint/package.py
+++ server_addon/tvpaint/package.py
@@ -1,3 +1,10 @@
 name = "tvpaint"
 title = "TVPaint"
-version = "0.1.2"
+version = "0.2.0"
+app_host_name = "tvpaint"
+client_dir = "ayon_tvpaint"
+
+ayon_required_addons = {
+    "core": ">0.3.1",
+}
+ayon_compatible_addons = {}
