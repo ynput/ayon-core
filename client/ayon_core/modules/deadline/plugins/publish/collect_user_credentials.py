23e26b07da3df4e3c210d1cdc590897062b8eb77
Petr Kalis <petr.kalis@gmail.com>
2024-05-27 18:09:19 +0200

    Carry over deadline information on render instance
    Was left behind before

--- client/ayon_core/modules/deadline/plugins/publish/collect_user_credentials.py
+++ client/ayon_core/modules/deadline/plugins/publish/collect_user_credentials.py
@@ -31,7 +31,8 @@ class CollectDeadlineUserCredentials(pyblish.api.InstancePlugin):
              "nuke",
              "maya",
              "max",
-             "houdini"]
+             "houdini",
+             "unreal"]
 
     families = ["render",
                 "rendering",
