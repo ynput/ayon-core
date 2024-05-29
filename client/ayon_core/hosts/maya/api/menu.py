c57c743b502df90612baab1ed6429ddef0dc7c6c
Kayla Man <moonyuet2008@hotmail.com>
2024-05-29 20:28:58 +0800

    make sure divider doesn't exist when the version up option is being disabled

--- client/ayon_core/hosts/maya/api/menu.py
+++ client/ayon_core/hosts/maya/api/menu.py
@@ -75,9 +75,8 @@ def install(project_settings):
 
         cmds.setParent("..", menu=True)
 
-        cmds.menuItem(divider=True)
-
         if project_settings["core"]["tools"]["Workfiles"].get("version_up_workfile"):
+            cmds.menuItem(divider=True)
             cmds.menuItem(
                 "Version Up Workfile",
                 command=lambda *args: version_up_workfile()
