599f6202478e09aaac2b498a3c803b232ab35704
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-05-31 11:13:46 +0200

    fix invalid imports

--- server_addon/photoshop/client/ayon_photoshop/hooks/pre_launch_args.py
+++ server_addon/photoshop/client/ayon_photoshop/hooks/pre_launch_args.py
@@ -7,7 +7,7 @@ from ayon_core.lib import (
     is_using_ayon_console,
 )
 from ayon_applications import PreLaunchHook, LaunchTypes
-from ayon_core.hosts.photoshop import get_launch_script_path
+from ayon_photoshop import get_launch_script_path
 
 
 def get_launch_kwargs(kwargs):
