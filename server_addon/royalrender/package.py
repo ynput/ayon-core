b44ed889b96bd85532c4e202814352f59833ef6d
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-06-05 18:23:01 +0200

    define client dir and added core dependency

--- server_addon/royalrender/package.py
+++ server_addon/royalrender/package.py
@@ -1,3 +1,10 @@
 name = "royalrender"
 title = "Royal Render"
 version = "0.2.0"
+
+client_dir = "ayon_royalrender"
+
+ayon_required_addons = {
+    "core": ">0.3.2",
+}
+ayon_compatible_addons = {}
