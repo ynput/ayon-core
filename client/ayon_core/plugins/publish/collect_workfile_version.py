dd99516853baadb1d97495449692f7662f79b762
Kayla Man <moonyuet2008@hotmail.com>
2024-06-05 13:43:54 +0800

    move the workfile version collector into general publish plugins

--- client/ayon_core/plugins/publish/collect_workfile_version.py
+++ client/ayon_core/plugins/publish/collect_workfile_version.py
@@ -5,15 +5,15 @@ from ayon_core.lib import get_version_from_path
 
 
 class CollectWorkfileVersion(pyblish.api.ContextPlugin):
-    """Inject the current working file into context"""
+    """Collect current workfile version as context data"""
 
     order = pyblish.api.CollectorOrder - 0.5
     label = "Current Workfile Version"
-    hosts = ["substancepainter"]
 
     def process(self, context):
         host = registered_host()
         path = host.get_current_workfile()
-        version = int(get_version_from_path(path))
-        context.data["version"] = version
-        self.log.debug(f"Current Version: {version}")
+        if path:
+            version = int(get_version_from_path(path))
+            context.data["version"] = version
+            self.log.debug(f"Current Version: {version}")
