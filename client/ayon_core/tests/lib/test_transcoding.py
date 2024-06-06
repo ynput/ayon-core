d1bcd4ef1b5d30ca82fa216a8229dd2d25c08311
Jakub Jezek <jakubjezek001@gmail.com>
2024-06-06 16:36:43 +0200

    Add get_image_info_metadata function for image metadata retrieval.
    - Added a new function to retrieve metadata from image files.
    - The function first tries OpenImageIO and then falls back to FFprobe if needed.

---
+++ client/ayon_core/tests/lib/test_transcoding.py
@@ -0,0 +1,53 @@
+import pytest
+import logging
+from pathlib import Path
+from ayon_core.lib.transcoding import get_image_info_metadata
+
+logger = logging.getLogger('test_transcoding')
+logger.setLevel(logging.DEBUG)
+
+
+@pytest.mark.parametrize(
+    "resources_path_factory, metadata, expected, test_id",
+    [
+        (
+            Path(__file__).parent.parent
+            / "resources"
+            / "lib"
+            / "transcoding"
+            / "a01vfxd_sh010_plateP01_v002.1013.exr",
+            ["timecode"],
+            {"timecode": "01:00:06:03"},
+            "test_01",
+        ),
+        (
+            Path(__file__).parent.parent
+            / "resources"
+            / "lib"
+            / "transcoding"
+            / "a01vfxd_sh010_plateP01_v002.1013.exr",
+            ["timecode", "width", "height", "duration"],
+            {"timecode": "01:00:06:03", "width": 1920, "height": 1080},
+            "test_02",
+        ),
+        (
+            Path(__file__).parent.parent
+            / "resources"
+            / "lib"
+            / "transcoding"
+            / "a01vfxd_sh010_plateP01_v002.mov",
+            ["width", "height", "duration"],
+            {"width": 1920, "height": 1080, "duration": "0.041708"},
+            "test_03",
+        ),
+    ],
+)
+def test_get_image_info_metadata_happy_path(
+    resources_path_factory, metadata, expected, test_id
+):
+    path_to_file = resources_path_factory.as_posix()
+
+    returned_data = get_image_info_metadata(path_to_file, metadata, logger)
+    logger.debug(f"Returned data: {returned_data}")
+
+    assert returned_data == expected
