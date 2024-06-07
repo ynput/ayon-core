60c42b4bbbcfe1b8ddf9b1e9bfb7262ef9723b62
Petr Kalis <petr.kalis@gmail.com>
2024-06-07 17:07:42 +0200

    Standardize families to submit_publish_job

--- server_addon/deadline/client/ayon_deadline/plugins/publish/collect_pools.py
+++ server_addon/deadline/client/ayon_deadline/plugins/publish/collect_pools.py
@@ -36,20 +36,13 @@ class CollectDeadlinePools(pyblish.api.InstancePlugin,
         "nuke",
     ]
 
-    families = ["render",
-                "prerender",
-                "rendering",
-                "render.farm",
-                "renderFarm",
-                "renderlayer",
-                "maxrender",
-                "usdrender",
-                "redshift_rop",
-                "arnold_rop",
-                "mantra_rop",
-                "karma_rop",
-                "vray_rop",
-                "publish.hou"]
+    families = ["render", "render.farm", "render.frames_farm",
+                "prerender", "prerender.farm", "prerender.frames_farm",
+                "renderlayer", "imagesequence", "image",
+                "vrayscene", "maxrender",
+                "arnold_rop", "mantra_rop",
+                "karma_rop", "vray_rop", "redshift_rop",
+                "renderFarm", "usrender", "publish.hou"]
 
     primary_pool = None
     secondary_pool = None
