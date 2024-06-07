d70c78ad3dff02d685a1c5ad44d5a75906f13b59
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-06-07 14:47:48 +0200

    removed duplicated attribute

--- server_addon/applications/package.py
+++ server_addon/applications/package.py
@@ -4,8 +4,6 @@ version = "0.2.3"
 
 client_dir = "ayon_applications"
 
-client_dir = "ayon_applications"
-
 ayon_server_version = ">=1.0.7"
 ayon_launcher_version = ">=1.0.2"
 ayon_required_addons = {
--- server_addon/maya/package.py
+++ server_addon/maya/package.py
@@ -1,9 +1,6 @@
 name = "maya"
 title = "Maya"
 version = "0.2.2"
-
-client_dir = "ayon_maya"
-
 client_dir = "ayon_maya"
 
 ayon_required_addons = {
