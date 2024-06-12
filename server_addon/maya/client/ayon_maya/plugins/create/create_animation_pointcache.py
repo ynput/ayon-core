820bb7318e51248237b0352e63c79e869faabf89
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-06-12 13:48:35 +0200

    don't call 'super' in 'get_instance_attr_defs'

--- server_addon/maya/client/ayon_maya/plugins/create/create_animation_pointcache.py
+++ server_addon/maya/client/ayon_maya/plugins/create/create_animation_pointcache.py
@@ -99,9 +99,7 @@ class CreateAnimation(plugin.MayaHiddenCreator):
         return node_data
 
     def get_instance_attr_defs(self):
-        defs = list(super().get_instance_attr_defs())
-        defs.extend(_get_animation_attr_defs(self))
-        return defs
+        return _get_animation_attr_defs(self)
 
 
 class CreatePointCache(plugin.MayaCreator):
@@ -123,9 +121,7 @@ class CreatePointCache(plugin.MayaCreator):
         return node_data
 
     def get_instance_attr_defs(self):
-        defs = list(super().get_instance_attr_defs())
-        defs.extend(_get_animation_attr_defs(self))
-        return defs
+        return _get_animation_attr_defs(self)
 
     def create(self, product_name, instance_data, pre_create_data):
         instance = super(CreatePointCache, self).create(
