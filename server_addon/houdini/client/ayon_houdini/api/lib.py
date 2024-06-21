981e2eae4d640e1defe2bd69fe6f9206d847155b
Roy Nieterau <roy_nieterau@hotmail.com>
2024-06-21 19:30:46 +0200

    Support `LabsKarma` node out of the box

--- server_addon/houdini/client/ayon_houdini/api/lib.py
+++ server_addon/houdini/client/ayon_houdini/api/lib.py
@@ -104,6 +104,8 @@ def get_output_parameter(node):
         return node.parm("outputimage")
     elif node_type == "vray_renderer":
         return node.parm("SettingsOutput_img_file_path")
+    elif node_type == "labs::karma::2.0":
+        return node.parm("file")
 
     raise TypeError("Node type '%s' not supported" % node_type)
 
