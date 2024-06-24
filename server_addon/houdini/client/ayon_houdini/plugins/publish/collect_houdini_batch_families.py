bfaf156a4e5ea84fc0a6f8ef3bebb373572f50af
Roy Nieterau <roy_nieterau@hotmail.com>
2024-06-24 20:29:24 +0200

    Remove simple runtime instance creator to separate for another PR

--- server_addon/houdini/client/ayon_houdini/plugins/publish/collect_houdini_batch_families.py
+++ server_addon/houdini/client/ayon_houdini/plugins/publish/collect_houdini_batch_families.py
@@ -17,21 +17,3 @@ class CollectNoProductTypeFamilyGeneric(pyblish.api.InstancePlugin):
         instance.data["family"] = "generic"
         # TODO: Do not add the dynamic 'rop' family in the collector?
         instance.data["families"] = ["generic", "rop"]
-        self.log.info("Generic..")
-
-
-class CollectNoProductTypeFamilyDynamic(pyblish.api.InstancePlugin):
-    """Collect data for caching to Deadline."""
-
-    order = pyblish.api.CollectorOrder - 0.49
-    families = ["dynamic"]
-    hosts = ["houdini"]
-    targets = ["local", "remote"]
-    label = "Collect Data for Cache"
-
-    def process(self, instance):
-        # Do not allow `productType` to creep into the pyblish families
-        # so that e.g. any regular plug-ins for `pointcache` or alike do
-        # not trigger.
-        instance.data["family"] = "dynamic"
-        instance.data["families"] = ["dynamic"]
