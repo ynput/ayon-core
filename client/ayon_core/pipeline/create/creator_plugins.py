33dfbf96f2f10590884aa2a3ca7dcf75e631071a
Roy Nieterau <roy_nieterau@hotmail.com>
2024-06-25 10:19:09 +0200

    Fix docstring argument

--- client/ayon_core/pipeline/create/creator_plugins.py
+++ client/ayon_core/pipeline/create/creator_plugins.py
@@ -455,7 +455,7 @@ class BaseCreator:
         'True' if did so.
 
         Args:
-            instance(List[CreatedInstance]): Instance objects which should be
+            instances (List[CreatedInstance]): Instance objects which should be
                 removed.
         """
 
