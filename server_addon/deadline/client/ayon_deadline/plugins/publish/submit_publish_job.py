817a7b73fb321cdb2d6b57ace81c839118cdd300
Petr Kalis <petr.kalis@gmail.com>
2024-07-04 12:25:42 +0200

    Use only AYON_IN_TESTS to drive automatic tests
    --automatic-tests is removed in AYON

--- server_addon/deadline/client/ayon_deadline/plugins/publish/submit_publish_job.py
+++ server_addon/deadline/client/ayon_deadline/plugins/publish/submit_publish_job.py
@@ -234,9 +234,6 @@ class ProcessSubmittedJobOnFarm(pyblish.api.InstancePlugin,
             "--targets", "farm"
         ]
 
-        if is_in_tests():
-            args.append("--automatic-tests")
-
         # Generate the payload for Deadline submission
         secondary_pool = (
             self.deadline_pool_secondary or instance.data.get("secondaryPool")
