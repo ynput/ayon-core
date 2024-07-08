db4599ce1c8576041e4931785dd3d1548b0eae97
Kayla Man <64118225+moonyuet@users.noreply.github.com>
2024-07-08 18:21:54 +0800

    Update server_addon/max/client/ayon_max/plugins/create/create_tyflow.py
    Co-authored-by: Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>

--- server_addon/max/client/ayon_max/plugins/create/create_tyflow.py
+++ server_addon/max/client/ayon_max/plugins/create/create_tyflow.py
@@ -5,7 +5,7 @@ from ayon_max.api import plugin
 
 class CreateTyFlow(plugin.MaxCacheCreator):
     """Creator plugin for TyFlow."""
-    identifier = "io.openpype.creators.max.tyflow"
+    identifier = "io.ayon.creators.max.tyflow"
     label = "TyFlow"
     product_type = "tyflow"
     icon = "gear"
