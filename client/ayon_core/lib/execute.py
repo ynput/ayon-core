232e21e88e0fbce5c739086bc85cc8d82d180d36
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-07-17 15:27:39 +0200

    'run_ayon_launcher_process' can add sys path to python path

--- client/ayon_core/lib/execute.py
+++ client/ayon_core/lib/execute.py
@@ -179,7 +179,7 @@ def clean_envs_for_ayon_process(env=None):
     return env
 
 
-def run_ayon_launcher_process(*args, **kwargs):
+def run_ayon_launcher_process(*args, add_sys_paths=False, **kwargs):
     """Execute AYON process with passed arguments and wait.
 
     Wrapper for 'run_process' which prepends AYON executable arguments
@@ -208,6 +208,14 @@ def run_ayon_launcher_process(*args, **kwargs):
         # Skip envs that can affect AYON launcher process
         # - fill more if you find more
         env = clean_envs_for_ayon_process(os.environ)
+
+    if add_sys_paths:
+        new_pythonpath = list(sys.path)
+        for path in env.get("PYTHONPATH", "").split(os.pathsep):
+            if not path or path in new_pythonpath:
+                continue
+            new_pythonpath.append(path)
+        env["PYTHONPATH"] = os.pathsep.join(new_pythonpath)
 
     return run_subprocess(args, env=env, **kwargs)
 
