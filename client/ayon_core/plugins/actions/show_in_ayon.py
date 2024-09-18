5090732cf8c6d0b104eeed4fbcd57bb3b93d2ebc
Roy Nieterau <roy_nieterau@hotmail.com>
2024-09-18 12:02:25 +0200

    Apply suggestions from code review
    Co-authored-by: Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>

--- client/ayon_core/plugins/actions/show_in_ayon.py
+++ client/ayon_core/plugins/actions/show_in_ayon.py
@@ -50,16 +50,14 @@ def get_ayon_entity_uri(
     return uris[0]["uri"]
 
 
-class ShowInAyon(LauncherAction):
+class ShowInAYON(LauncherAction):
     """Open AYON browser page to the current context."""
     name = "showinayon"
     label = "Show in AYON"
     icon = get_ayon_icon_filepath()
-    color = "#e0e1e1"
     order = 999
 
     def process(self, selection, **kwargs):
-
         url = os.environ["AYON_SERVER_URL"]
         if selection.is_project_selected:
             project_name = selection.project_name
@@ -86,6 +84,4 @@ class ShowInAyon(LauncherAction):
 
         # Open URL in webbrowser
         self.log.info(f"Opening URL: {url}")
-        webbrowser.open(url,
-                        # Try in new tab
-                        new=2)
+        webbrowser.open_new_tab(url)
