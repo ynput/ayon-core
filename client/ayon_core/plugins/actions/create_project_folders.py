d7bb8c0b3d759d3d9daadd1a8583fd6d9253d041
Roy Nieterau <roy_nieterau@hotmail.com>
2024-10-04 11:12:03 +0200

    Hide action if setting is empty

--- client/ayon_core/plugins/actions/create_project_folders.py
+++ client/ayon_core/plugins/actions/create_project_folders.py
@@ -10,6 +10,16 @@ class CreateProjectFoldersAction(LauncherAction):
     order = 1000
 
     def is_compatible(self, selection) -> bool:
+
+        # Disable when the project folder structure setting is empty
+        # in settings
+        project_settings = selection.get_project_settings()
+        folder_structure = (
+            project_settings["core"]["project_folder_structure"]
+        ).strip()
+        if not folder_structure or folder_structure == "{}":
+            return False
+
         return (
             selection.is_project_selected
             and not selection.is_folder_selected
