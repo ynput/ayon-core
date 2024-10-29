dc9e49333100299dc9bd8196fb5babd8bea9ed16
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-10-29 11:31:00 +0100

    remove space between version and dot

--- client/ayon_core/pipeline/delivery.py
+++ client/ayon_core/pipeline/delivery.py
@@ -384,7 +384,7 @@ def get_representations_delivery_template_data(
 
         template_data = repre_entity["context"]
         # Bug in 'ayon_api', 'get_representations_hierarchy' did not fully
-        #   convert representation entity. Fixed in 'ayon_api' 1.0.10 .
+        #   convert representation entity. Fixed in 'ayon_api' 1.0.10.
         if isinstance(template_data, str):
             con = ayon_api.get_server_api_connection()
             repre_entity = con._representation_conversion(repre_entity)
