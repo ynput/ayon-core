1dd3c00eb5802254a21bd5d7ea51f562a93ac366
Ondrej Samohel <ondrej.samohel@ynput.io>
2024-12-10 23:02:10 +0100

    :bug: fix template data

--- tests/client/ayon_core/plugins/publish/test_integrate_traits.py
+++ tests/client/ayon_core/plugins/publish/test_integrate_traits.py
@@ -2,10 +2,16 @@
 from __future__ import annotations
 
 import base64
+import time
 from pathlib import Path
 
 import pyblish.api
 import pytest
+import pytest_ayon
+from ayon_api.operations import (
+    OperationsSession,
+)
+from ayon_core.pipeline.anatomy import Anatomy
 from ayon_core.pipeline.traits import (
     FileLocation,
     FileLocations,
@@ -18,6 +24,7 @@ from ayon_core.pipeline.traits import (
     Sequence,
     Transient,
 )
+from ayon_core.pipeline.version_start import get_versioning_start
 
 # Tagged,
 # TemplatePath,
@@ -26,6 +33,8 @@ from ayon_core.settings import get_project_settings
 
 PNG_FILE_B64 = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQAAAAA3bvkkAAAACklEQVR4AWNgAAAAAgABc3UBGAAAAABJRU5ErkJggg=="  # noqa: E501
 SEQUENCE_LENGTH = 10
+CURRENT_TIME = time.time()
+
 
 @pytest.fixture(scope="session")
 def single_file(tmp_path_factory: pytest.TempPathFactory) -> Path:
@@ -48,25 +57,13 @@ def sequence_files(tmp_path_factory: pytest.TempPathFactory) -> list[Path]:
 
 @pytest.fixture()
 def mock_context(
-        project: object,
+        project: pytest_ayon.ProjectInfo,
         single_file: Path,
         sequence_files: list[Path]) -> pyblish.api.Context:
     """Return a mock instance.
 
     This is mocking pyblish context for testing. It is using real AYON project
     thanks to the ``project`` fixture.
-
-    This returns following data::
-
-        project_name: str
-        project_code: str
-        project_root_folders: dict[str, str]
-        folder: IdNamePair
-        task: IdNamePair
-        product: IdNamePair
-        version: IdNamePair
-        representations: List[IdNamePair]
-        links: List[str]
 
     Args:
         project (object): The project info. It is `ProjectInfo` object
@@ -75,24 +72,67 @@ def mock_context(
         sequence_files (list[Path]): The paths to a sequence of image files.
 
     """
+    anatomy = Anatomy(project.project_name)
     context = pyblish.api.Context()
     context.data["projectName"] = project.project_name
     context.data["hostName"] = "test_host"
     context.data["project_settings"] = get_project_settings(
         project.project_name)
+    context.data["anatomy"] = anatomy
+    context.data["time"] = CURRENT_TIME
+    context.data["user"] = "test_user"
+    context.data["machine"] = "test_machine"
+    context.data["fps"] = 25
 
     instance = context.create_instance("mock_instance")
+    instance.data["source"] = "test_source"
+    instance.data["families"] = ["render"]
     instance.data["anatomyData"] = {
-        "project": project.project_name,
+        "project": {
+            "name": project.project_name,
+            "code": project.project_code
+        },
         "task": {
             "name": project.task.name,
             "type": "test"  # pytest-ayon doesn't return the task type yet
-        }
+        },
+        "folder": {
+            "name": project.folder.name,
+            "type": "test"  # pytest-ayon doesn't return the folder type yet
+        },
+        "product": {
+            "name": project.product.name,
+            "type": "test"  # pytest-ayon doesn't return the product type yet
+        },
+
     }
+    instance.data["folderEntity"] = project.folder_entity
     instance.data["productType"] = "test_product"
+    instance.data["productName"] = project.product.name
+    instance.data["anatomy"] = anatomy
+    instance.data["comment"] = "test_comment"
 
     instance.data["integrate"] = True
     instance.data["farm"] = False
+
+    parents = project.folder_entity["path"].lstrip("/").split("/")
+
+    hierarchy = ""
+    if parents:
+        hierarchy = "/".join(parents)
+
+    instance.data["hierarchy"] = hierarchy
+
+    version_number = get_versioning_start(
+        context.data["projectName"],
+        instance.context.data["hostName"],
+        task_name=project.task.name,
+        task_type="test",
+        product_type=instance.data["productType"],
+        product_name=instance.data["productName"]
+    )
+
+    instance.data["version"] = version_number
 
     _file_size = len(base64.b64decode(PNG_FILE_B64))
     file_locations = [
@@ -121,7 +161,7 @@ def mock_context(
             ),
             Sequence(
                 frame_padding=4,
-                frame_regex=r"^img\.(\d{4})\.png$",
+                frame_regex=r"^img\.(?P<frame>\d{4})\.png$",
             ),
             FileLocations(
                 file_paths=file_locations,
@@ -176,3 +216,87 @@ def test_filter_lifecycle() -> None:
 
     assert len(filtered) == 1
     assert filtered[0] == persistent_representation
+
+
+def test_prepare_product(
+        project: pytest_ayon.ProjectInfo,
+        mock_context: pyblish.api.Context) -> None:
+    """Test prepare_product."""
+    integrator = IntegrateTraits()
+    op_session = OperationsSession()
+    product = integrator.prepare_product(mock_context[0], op_session)
+
+    assert product == {
+        "attrib": {},
+        "data": {
+            "families": ["default", "render"],
+            },
+        "folderId": project.folder_entity["id"],
+        "name": "renderMain",
+        "productType": "test_product",
+        "id": project.product_entity["id"],
+        }
+
+def test_prepare_version(
+        project: pytest_ayon.ProjectInfo,
+        mock_context: pyblish.api.Context) -> None:
+    """Test prepare_version."""
+    integrator = IntegrateTraits()
+    op_session = OperationsSession()
+    product = integrator.prepare_product(mock_context[0], op_session)
+    version = integrator.prepare_version(
+        mock_context[0], op_session , product)
+
+    assert version == {
+        "attrib": {
+            "comment": "test_comment",
+            "families": ["default", "render"],
+            "fps": 25,
+            "machine": "test_machine",
+            "source": "test_source",
+        },
+        "data": {
+            "author": "test_user",
+            "time": CURRENT_TIME,
+        },
+        "id": project.version_entity["id"],
+        "productId": project.product_entity["id"],
+        "version": 1,
+    }
+
+
+def test_get_transfers_from_representation(
+        mock_context: pyblish.api.Context) -> None:
+    """Test get_transfers_from_representation."""
+    integrator = IntegrateTraits()
+
+    instance = mock_context[0]
+    representations: list[Representation] = instance.data[
+        "representations_with_traits"]
+    transfers = integrator.get_transfers_from_representations(
+        instance, representations)
+
+    assert transfers == [
+        {
+            "file_path": Path("test"),
+            "file_size": 1234,
+            "traits": [
+                "Persistent",
+                "Image",
+                "MimeType"
+            ]
+        },
+        {
+            "file_path": Path("test"),
+            "file_size": 1234,
+            "traits": [
+                "Persistent",
+                "FrameRanged",
+                "Sequence",
+                "FileLocations",
+                "Image",
+                "PixelBased",
+                "MimeType"
+            ]
+        }
+    ]
