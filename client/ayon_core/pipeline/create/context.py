b75358b0e282fadc224b23b01d2a5f77fe18040a
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2025-02-04 10:29:01 +0100

    added helper method 'get_template_data' to create context

--- client/ayon_core/pipeline/create/context.py
+++ client/ayon_core/pipeline/create/context.py
@@ -29,6 +29,7 @@ from ayon_core.lib.events import QueuedEventSystem
 from ayon_core.lib.attribute_definitions import get_default_values
 from ayon_core.host import IPublishHost, IWorkfileHost
 from ayon_core.pipeline import Anatomy
+from ayon_core.pipeline.template_data import get_template_data
 from ayon_core.pipeline.plugin_discover import DiscoverResult
 
 from .exceptions import (
@@ -479,6 +480,36 @@ class CreateContext:
             self._current_project_settings = get_project_settings(
                 self.get_current_project_name())
         return self._current_project_settings
+
+    def get_template_data(
+        self, folder_path: Optional[str], task_name: Optional[str]
+    ) -> dict[str, Any]:
+        """Prepare template data for given context.
+
+        Method is using cached entities and settings to prepare template data.
+
+        Args:
+            folder_path (Optional[str]): Folder path.
+            task_name (Optional[str]): Task name.
+
+        Returns:
+            dict[str, Any]: Template data.
+
+        """
+        project_entity = self.get_current_project_entity()
+        folder_entity = task_entity = None
+        if folder_path:
+            folder_entity = self.get_folder_entity(folder_path)
+            if task_name and folder_entity:
+                task_entity = self.get_task_entity(folder_path, task_name)
+
+        return get_template_data(
+            project_entity,
+            folder_entity,
+            task_entity,
+            host_name=self.host_name,
+            settings=self.get_current_project_settings(),
+        )
 
     @property
     def context_has_changed(self):
