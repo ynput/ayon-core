6913b3be69cac92d4725ebe0075b1b2783f7cc96
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2025-02-13 14:42:45 +0100

    fill only platform root

--- client/ayon_core/pipeline/anatomy/roots.py
+++ client/ayon_core/pipeline/anatomy/roots.py
@@ -23,16 +23,19 @@ class RootItem(FormatObject):
     def __init__(self, parent, root_raw_data, name):
         super(RootItem, self).__init__()
         self._log = None
-        lowered_platform_keys = {}
-        for key, value in root_raw_data.items():
-            lowered_platform_keys[key.lower()] = value.format(**os.environ)
+        lowered_platform_keys = {
+            key.lower(): value
+            for key, value in root_raw_data.items()
+        }
         self.raw_data = lowered_platform_keys
         self.cleaned_data = self._clean_roots(lowered_platform_keys)
         self.name = name
         self.parent = parent
 
         self.available_platforms = set(lowered_platform_keys.keys())
-        self.value = lowered_platform_keys.get(platform.system().lower())
+        self.value = lowered_platform_keys[platform.system().lower()].format(
+            **os.environ
+        )
         self.clean_value = self._clean_root(self.value)
 
     def __format__(self, *args, **kwargs):
@@ -105,10 +108,10 @@ class RootItem(FormatObject):
 
     def _clean_roots(self, raw_data):
         """Clean all values of raw root item values."""
-        cleaned = {}
-        for key, value in raw_data.items():
-            cleaned[key] = self._clean_root(value)
-        return cleaned
+        return {
+            key: self._clean_root(value)
+            for key, value in raw_data.items()
+        }
 
     def path_remapper(self, path, dst_platform=None, src_platform=None):
         """Remap path for specific platform.
