cf1a676fe33c3708827adb324c02a3fa383519d7
Roy Nieterau <roy_nieterau@hotmail.com>
2025-03-10 16:13:38 +0100

    Fix duplicate workfile from published

--- client/ayon_core/tools/workfiles/abstract.py
+++ client/ayon_core/tools/workfiles/abstract.py
@@ -1060,7 +1060,7 @@ class AbstractWorkfilesFrontend(AbstractWorkfilesCommon):
         pass
 
     @abstractmethod
-    def duplicate_workfile(self, src_filepath, workdir, filename):
+    def duplicate_workfile(self, src_filepath, workdir, filename, artist_note):
         """Duplicate workfile.
 
         Workfiles is not opened when done.
@@ -1069,6 +1069,7 @@ class AbstractWorkfilesFrontend(AbstractWorkfilesCommon):
             src_filepath (str): Source workfile path.
             workdir (str): Destination workdir.
             filename (str): Destination filename.
+            artist_note (str): Artist note.
         """
 
         pass
