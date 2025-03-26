f9781cf19c30886a7f66a9ba08a8fd313a3b6074
Krishna LBSFX <vudathasai2@gmail.com>
2025-03-26 17:23:22 +0530

    new logos added

--- client/ayon_core/resources/__init__.py
+++ client/ayon_core/resources/__init__.py
@@ -40,16 +40,16 @@ def get_liberation_font_path(bold=False, italic=False):
 
 
 def get_ayon_production_icon_filepath():
-    return get_resource("icons", "AYON_icon.png")
+    return get_resource("icons", "black_production.png")
 
 
 def get_ayon_staging_icon_filepath():
-    return get_resource("icons", "AYON_icon_staging.png")
+    return get_resource("icons", "black_staging.png")
 
 
 def get_ayon_icon_filepath(staging=None):
     if os.getenv("AYON_USE_DEV") == "1":
-        return get_resource("icons", "AYON_icon_dev.png")
+        return get_resource("icons", "black_developer.png")
 
     if staging is None:
         staging = is_staging_enabled()
