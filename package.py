47583fb1ef520c15a6e97fae4898c738cd6fae39
Jakub Jezek <jakubjezek001@gmail.com>
2025-04-02 15:00:44 +0200

    Update OCIO config paths and compatible addons
    - Added a new compatible addon with version requirement.
    - Updated existing OCIO config paths for ACES 1.3 and added support for ACES 2.0.
    - Adjusted labels and descriptions for clarity on OCIO versions.

--- package.py
+++ package.py
@@ -9,4 +9,6 @@ plugin_for = ["ayon_server"]
 ayon_server_version = ">=1.0.3,<2.0.0"
 ayon_launcher_version = ">=1.0.2"
 ayon_required_addons = {}
-ayon_compatible_addons = {}
+ayon_compatible_addons = {
+    "ayon_ocio": ">=1.2.0",
+}
