662a3ea201913044625e6ba298013e1ec6825799
Juan M <166030421+jm22dogs@users.noreply.github.com>
2025-04-04 14:42:45 +0100

    Update client/ayon_core/tools/publisher/widgets/card_view_widgets.py
    Co-authored-by: Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>

--- client/ayon_core/tools/publisher/widgets/card_view_widgets.py
+++ client/ayon_core/tools/publisher/widgets/card_view_widgets.py
@@ -529,7 +529,9 @@ class InstanceCardWidget(CardWidget):
             for part in found_parts:
                 replacement = "<b>{}</b>".format(part)
                 label = label.replace(part, replacement)
-                label += self.get_card_widget_sub_label(folder_name, task_name)
+                sublabel = self._get_card_widget_sub_label(folder_path, task_name)
+                if sublabel:
+                    label += f"<br/>{sublabel}"
 
         self._label_widget.setText(label)
         # HTML text will cause that label start catch mouse clicks
