a9e47977f8d07a90c88069ce080f216cf63b5ae6
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2025-04-25 17:12:18 +0200

    moved interfaces to subfolder

---
+++ client/ayon_core/host/interfaces/__init__.py
@@ -0,0 +1,16 @@
+from .interfaces import (
+    MissingMethodsError,
+    IPublishHost,
+    INewPublisher,
+    ILoadHost,
+    IWorkfileHost,
+)
+
+
+__all__ = (
+    "MissingMethodsError",
+    "IWorkfileHost",
+    "IPublishHost",
+    "INewPublisher",
+    "ILoadHost",
+)
