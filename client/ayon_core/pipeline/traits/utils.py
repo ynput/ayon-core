6f127b2b6161b6aa60fae7f564536ef24302dc98
Ondřej Samohel <ondrej.samohel@ynput.io>
2025-05-13 12:20:43 +0200

    :dog: fix ruff

--- client/ayon_core/pipeline/traits/utils.py
+++ client/ayon_core/pipeline/traits/utils.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Optional
 
-import pyblish.api
 from clique import assemble
 
 from ayon_core.addon import AddonsManager, ITraits
