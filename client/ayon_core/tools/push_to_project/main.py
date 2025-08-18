ef9ab3bcdc106854472608a5a0772888ed128089
Petr Kalis <petr.kalis@gmail.com>
2025-08-18 13:31:31 +0200

    Changed main input to accept multiple version ids

--- client/ayon_core/tools/push_to_project/main.py
+++ client/ayon_core/tools/push_to_project/main.py
@@ -4,28 +4,28 @@ from ayon_core.tools.utils import get_ayon_qt_app
 from ayon_core.tools.push_to_project.ui import PushToContextSelectWindow
 
 
-def main_show(project_name, version_id):
+def main_show(project_name, version_ids):
     app = get_ayon_qt_app()
 
     window = PushToContextSelectWindow()
     window.show()
-    window.set_source(project_name, version_id)
+    window.set_source(project_name, version_ids)
 
     app.exec_()
 
 
 @click.command()
 @click.option("--project", help="Source project name")
-@click.option("--version", help="Source version id")
-def main(project, version):
+@click.option("--versions", help="Source version ids")
+def main(project, versions):
     """Run PushToProject tool to integrate version in different project.
 
     Args:
         project (str): Source project name.
-        version (str): Version id.
+        versions (str): comma separated versions for same context
     """
 
-    main_show(project, version)
+    main_show(project, versions)
 
 
 if __name__ == "__main__":
