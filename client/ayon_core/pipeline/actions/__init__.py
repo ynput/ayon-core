895a7f17910790769cd3f8a29c6fc4fa874cba1c
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2025-08-20 15:21:40 +0200

    moved current actions to subdir

---
+++ client/ayon_core/pipeline/actions/__init__.py
@@ -0,0 +1,33 @@
+from .launcher import (
+    LauncherAction,
+    LauncherActionSelection,
+    discover_launcher_actions,
+    register_launcher_action,
+    register_launcher_action_path,
+)
+
+from .inventory import (
+    InventoryAction,
+    discover_inventory_actions,
+    register_inventory_action,
+    register_inventory_action_path,
+
+    deregister_inventory_action,
+    deregister_inventory_action_path,
+)
+
+
+__all__= (
+    "LauncherAction",
+    "LauncherActionSelection",
+    "discover_launcher_actions",
+    "register_launcher_action",
+    "register_launcher_action_path",
+
+    "InventoryAction",
+    "discover_inventory_actions",
+    "register_inventory_action",
+    "register_inventory_action_path",
+    "deregister_inventory_action",
+    "deregister_inventory_action_path",
+)
