263da4870c0edf9879f21be2b534914345c2cdf1
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2025-08-21 15:10:03 +0200

    added 'get_loader_action_plugin_paths' to 'IPluginPaths'

--- client/ayon_core/addon/interfaces.py
+++ client/ayon_core/addon/interfaces.py
@@ -185,6 +185,10 @@ class IPluginPaths(AYONInterface):
         """
         return self._get_plugin_paths_by_type("inventory")
 
+    def get_loader_action_plugin_paths(self) -> list[str]:
+        """Receive loader action plugin paths."""
+        return []
+
 
 class ITrayAddon(AYONInterface):
     """Addon has special procedures when used in Tray tool.
