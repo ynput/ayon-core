257dfe204a62dedf4a88d598bbba54cd133cf5eb
Ondřej Samohel <ondrej.samohel@ynput.io>
2025-09-08 14:35:57 +0200

    :sparkles: resolve environments in dev bundle paths

--- client/ayon_core/addon/base.py
+++ client/ayon_core/addon/base.py
@@ -302,6 +302,9 @@ def _load_ayon_addons(log):
         milestone_version = MOVED_ADDON_MILESTONE_VERSIONS.get(addon_name)
         if use_dev_path:
             addon_dir = dev_addon_info["path"]
+            if addon_dir:
+                addon_dir.format(os.environ)
+
             if not addon_dir or not os.path.exists(addon_dir):
                 log.warning((
                     "Dev addon {} {} path does not exists. Path \"{}\""
