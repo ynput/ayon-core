b250b5fec4ac627917c744828d166c71eafb2ffe
MustafaJafar <mustafataherzaky@outlook.com>
2026-01-10 04:31:42 +0200

    `do_profile`: only profile under a condition

--- client/ayon_core/lib/profiling.py
+++ client/ayon_core/lib/profiling.py
@@ -5,18 +5,29 @@ import cProfile
 import functools
 
 
-def do_profile(to_file=None):
+def do_profile(to_file=None, tool_name=None):
     """Wraps function in profiler run and print stat after it is done.
 
     Args:
         to_file (str, optional): If specified, dumps stats into the file
-        instead of printing.
-
+            instead of printing.
+        tool_name (str, optional): A given name to your function. It will
+            be checked against the 'ENABLE_PROFILING' environment variable.
+            If None, the filtering is skipped and the function is
+            always profiled.
     """
     if to_file:
         to_file = to_file.format(pid=os.getpid())
 
     def _do_profile(fn):
+        env_val = os.getenv("AYON_ENABLE_PROFILING", "ALL").strip()
+        if (
+            tool_name and
+            env_val != "ALL" and
+            tool_name != env_val
+        ):
+            return fn
+
         @functools.wraps(fn)
         def profiled(*args, **kwargs):
             profiler = cProfile.Profile()
