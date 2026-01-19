fdf98f75acc7be10e3f0770aa7f8ac49f9e5004c
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-01-19 17:10:39 +0100

    fix used method in overview widget

--- client/ayon_core/tools/publisher/widgets/overview_widget.py
+++ client/ayon_core/tools/publisher/widgets/overview_widget.py
@@ -321,8 +321,9 @@ class OverviewWidget(QtWidgets.QFrame):
 
         # Disable delete button if nothing is selected
         self._delete_btn.setEnabled(len(instance_ids) > 0)
-
-        instances_by_id = self._controller.get_instances_by_id(instance_ids)
+        instances_by_id = self._controller.get_instance_items_by_id(
+            instance_ids
+        )
         instances = [
             instances_by_id[instance_id]
             for instance_id in instance_ids
