fd4584893dd5516ee75b3aaabf6dbe21ac7ab7ea
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-01-29 10:37:52 +0100

    support both product type and product base type

--- client/ayon_core/pipeline/version_start.py
+++ client/ayon_core/pipeline/version_start.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import functools
 import warnings
 from typing import Optional, Any
 
@@ -11,48 +10,29 @@ from ayon_core.settings import get_project_settings
 log = Logger.get_logger(__name__)
 
 
-def _get_versioning_start_wrap(func):
-    """Change 'product_type' kwarg to 'product_base_type' compatibility."""
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        if "product_type" in kwargs:
-            msg = (
-                "Found 'product_type' kwarg in 'get_versioning_start',"
-                " use 'product_base_type' instead."
-            )
-            log.warning(msg)
-            warnings.warn(msg, DeprecationWarning, stacklevel=2)
-
-            kwargs["product_base_type"] = kwargs.pop("product_type")
-
-        if len(args) > 6:
-            msg = (
-                "Got more positional arguments for 'get_versioning_start'"
-                " than expected. Please use explicit kwargs instead."
-            )
-            log.warning(msg)
-            warnings.warn(msg, DeprecationWarning, stacklevel=2)
-            args, rem = args[:6], args[6:]
-            kwargs["project_settings"] = rem[0]
-
-        return func(*args, **kwargs)
-    return wrapper
-
-
-@_get_versioning_start_wrap
 def get_versioning_start(
     project_name: str,
     host_name: str,
     task_name: Optional[str] = None,
     task_type: Optional[str] = None,
-    product_base_type: Optional[str] = None,
+    product_type: Optional[str] = None,
     product_name: Optional[str] = None,
+    product_base_type: Optional[str] = None,
     *,
     project_settings: Optional[dict[str, Any]] = None,
 ) -> int:
     """Get anatomy versioning start"""
     if not project_settings:
         project_settings = get_project_settings(project_name)
+
+    if product_base_type is None and product_type:
+        msg = (
+            "Found 'product_type' kwarg in 'get_versioning_start',"
+            " use 'product_base_type' instead."
+        )
+        log.warning(msg)
+        warnings.warn(msg, DeprecationWarning, stacklevel=2)
+        product_base_type = product_type
 
     version_start = 1
     settings = project_settings["core"]
