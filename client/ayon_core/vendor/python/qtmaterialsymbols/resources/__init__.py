7ebf3cbd0940b724933d3ce1f17101d7c6f29dd2
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-02-20 10:40:26 +0100

    update qtmateriasymbols

--- client/ayon_core/vendor/python/qtmaterialsymbols/resources/__init__.py
+++ client/ayon_core/vendor/python/qtmaterialsymbols/resources/__init__.py
@@ -5,12 +5,12 @@ CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def get_font_filepath(
-    font_name: Optional[str] = "MaterialSymbolsOutlined"
+    font_name: Optional[str] = "MaterialSymbolsOutlined[FILL,GRAD,opsz,wght]"
 ) -> str:
     return os.path.join(CURRENT_DIR, f"{font_name}.ttf")
 
 
 def get_mapping_filepath(
-    font_name: Optional[str] = "MaterialSymbolsOutlined"
+    font_name: Optional[str] = "MaterialSymbolsOutlined[FILL,GRAD,opsz,wght]"
 ) -> str:
     return os.path.join(CURRENT_DIR, f"{font_name}.json")
