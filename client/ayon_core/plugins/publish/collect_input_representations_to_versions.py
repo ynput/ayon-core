530e7a1bd6750e7333ec1893153980bc06855d17
Roy Nieterau <roy_nieterau@hotmail.com>
2026-03-11 17:47:50 +0100

    Simplify remapping logic

--- client/ayon_core/plugins/publish/collect_input_representations_to_versions.py
+++ client/ayon_core/plugins/publish/collect_input_representations_to_versions.py
@@ -37,23 +37,14 @@ class CollectInputRepresentationsToVersions(pyblish.api.ContextPlugin):
             representation_ids=representations,
             fields={"id", "versionId"})
 
-        representation_id_to_version_id = {
+        version_info_by_repre_id = {
             repre["id"]: InputVersion(version_id=repre["versionId"])
             for repre in repre_entities
         }
 
-        # Remap hero versions to their 'real' version ids
-        hero_versions = list(ayon_api.get_hero_versions(
-            project_name=project_name,
-            version_ids={
-                v.version_id for v in representation_id_to_version_id.values()
-            },
-            fields={"id", "productId", "version"}
-        ))
         # Replace hero versions with their real versions
-        representation_id_to_version_id = self.hero_versions_to_versions(
-            representation_id_to_version_id,
-            hero_versions,
+        version_info_by_repre_id = self.hero_versions_to_versions(
+            version_info_by_repre_id,
             project_name,
         )
 
@@ -64,7 +55,7 @@ class CollectInputRepresentationsToVersions(pyblish.api.ContextPlugin):
 
             input_versions = instance.data.setdefault("inputVersions", [])
             for repre_id in inst_repre:
-                version_id = representation_id_to_version_id.get(repre_id)
+                version_id = version_info_by_repre_id.get(repre_id)
                 if version_id:
                     input_versions.append(version_id)
                 else:
@@ -77,49 +68,68 @@ class CollectInputRepresentationsToVersions(pyblish.api.ContextPlugin):
 
     def hero_versions_to_versions(
             self,
-            representation_id_to_version_id: dict[str, InputVersion],
-            hero_versions: list[dict],
+            version_info_by_repre_id: dict[str, InputVersion],
             project_name: str,
     ) -> dict[str, InputVersion]:
+        """Remap hero versions to their 'real' version ids"""
+        # Get all hero version ids among the versions
+        hero_versions: list[dict] = list(ayon_api.get_hero_versions(
+            project_name=project_name,
+            version_ids={
+                v.version_id for v in version_info_by_repre_id.values()
+            },
+            fields={"id", "productId"},
+        ))
         if not hero_versions:
-            return representation_id_to_version_id
+            return version_info_by_repre_id
 
         product_ids = {hv["productId"] for hv in hero_versions}
+        hero_version_ids: set[str] = {hv["id"] for hv in hero_versions}
+
+        # TODO: When backend supports it filter directly to only versions
+        #  where hero version id matches our list of versions or skip those
+        #  that have no hero version id
         versions = ayon_api.get_versions(
             project_name,
-            product_ids=product_ids
+            product_ids=product_ids,
+            fields={"id", "heroVersionId"}
         )
-        version_id_by_product_id_and_version: dict[tuple[str, int], str] = {
-            (v["productId"], v["version"]): v["id"] for v in versions
+        # Mapping from hero version id to real version id
+        version_id_by_hero_version_id: dict[str, str] = {
+            v["heroVersionId"]: v["id"] for v in versions
+            # Disregard versions with irrelevant hero version id
+            if v["heroVersionId"] in hero_version_ids
         }
+        self.log.debug(
+            f"Hero version to version id map: {version_id_by_hero_version_id}"
+        )
 
-        for hero_version in hero_versions:
-            real_key: tuple[str, int] = (
-                hero_version["productId"],
-                # Hero version uses negative version numbers of
-                # their real versions
-                abs(hero_version["version"])
+        # Update mapping to point to real version id
+        for repre_id, input_version in tuple(
+            version_info_by_repre_id.items()
+        ):
+            # Nothing to remap if not a hero version
+            if input_version.version_id not in hero_version_ids:
+                continue
+
+            # Get real version id for hero version id
+            hero_version_id: str = input_version.version_id
+            real_version_id: str = version_id_by_hero_version_id.get(
+                hero_version_id
             )
-            real_version_id = version_id_by_product_id_and_version.get(
-                real_key)
             if not real_version_id:
                 self.log.debug(
                     "Could not find real version for hero version: %s.",
-                    hero_version["id"],
+                    hero_version_id,
                 )
                 continue
 
-            # Update mapping to point to real version id
-            for repre_id, input_version in tuple(
-                representation_id_to_version_id.items()
-            ):
-                if input_version.version_id == hero_version["id"]:
-                    representation_id_to_version_id[repre_id] = (
-                        InputVersion(
-                            version_id=real_version_id,
-                            hero=True,
-                            hero_version_id=hero_version["id"],
-                        )
-                    )
+            version_info_by_repre_id[repre_id] = (
+                InputVersion(
+                    version_id=real_version_id,
+                    hero=True,
+                    hero_version_id=hero_version_id,
+                )
+            )
 
-        return representation_id_to_version_id
+        return version_info_by_repre_id
