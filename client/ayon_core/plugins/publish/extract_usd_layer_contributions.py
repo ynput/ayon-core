683d1e66bf00f41fb08d347e4f0e464bd5e1225f
Mustafa Zaky Jafar <mustafataherzaky@outlook.com>
2026-04-21 23:12:04 +0200

    USD Contribution: Add task names to profile filters

--- client/ayon_core/plugins/publish/extract_usd_layer_contributions.py
+++ client/ayon_core/plugins/publish/extract_usd_layer_contributions.py
@@ -530,10 +530,12 @@ class CollectUSDLayerContributions(pyblish.api.InstancePlugin,
         product_base_type = instance.data.get("productBaseType")
         if not product_base_type:
             product_base_type = instance.data["productType"]
-        profile = filter_profiles(cls.profiles, {
+        filtering_criteria = {
             "product_base_types": product_base_type,
-            "task_types": current_context_task_type
-        })
+            "task_types": current_context_task_type,
+            "task_names": create_context.get_current_task_name()
+        }
+        profile = filter_profiles(cls.profiles, filtering_criteria)
         if not profile:
             profile = {}
 
