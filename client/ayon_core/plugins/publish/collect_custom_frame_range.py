a3535b282428bc064d7d96f61a786cd6b0d623ba
Kayla Man <moonyuet2008@hotmail.com>
2026-05-08 21:43:23 +0800

    move custom frame range option into seperate collector

---
+++ client/ayon_core/plugins/publish/collect_custom_frame_range.py
@@ -0,0 +1,117 @@
+"""Collect custom frame range for render submission."""
+import pyblish.api
+from typing import Optional
+from ayon_core.lib import EnumDef, TextDef
+from ayon_core.pipeline import KnownPublishError
+from ayon_core.pipeline.publish import AYONPyblishPluginMixin
+
+
+class CollectCustomFrameRange(pyblish.api.InstancePlugin,
+                              AYONPyblishPluginMixin):
+    """Collect custom frame range for render submission."""
+
+    order = pyblish.api.CollectorOrder + 0.018
+    label = "Collect Custom Frame Range"
+
+    def process(self, instance: pyblish.api.Instance) -> None:
+        attr_values = self.get_attr_values_from_data(instance.data)
+        if not self._is_custom_frames_used(
+            attr_values.get("use_custom_frames")
+        ):
+            self.log.debug(
+                "Custom frames are not used, "
+                "skipping collection of frame range."
+            )
+            return
+        if not attr_values["frames"]:
+            raise KnownPublishError("Please fill `Custom Frames` value")
+        instance.data["custom_frames"] = attr_values["frames"]
+        if attr_values["use_custom_frames"] == "reuse_last_version":
+            instance.data["reuse_last_version"] = True
+
+    @classmethod
+    def get_attr_defs_for_instance(cls, create_context, instance):
+        """Get list of attr defs that are set in Settings as artist overridable
+
+        Args:
+            create_context (ayon_core.pipeline.create.CreateContext)
+            instance (ayon_core.pipeline.create.CreatedInstance):
+
+        Returns:
+            (list)
+        """
+        if not cls.instance_matches_plugin_families(instance):
+            return []
+        defs = []
+        use_custom_frames = (
+            cls._get_publish_use_custom_frames_value(instance.data) or "none"
+        )
+
+        # explicit frames to render - for test renders
+        use_custom_frames_enum_values = [
+            {"value": "none", "label": "Disabled"},
+            {"value": "custom_only", "label": "Custom Frames Only"},
+            {"value": "reuse_last_version", "label": "Reuse from Last Version"}
+        ]
+        defs.append(
+            EnumDef(
+                "use_custom_frames",
+                label="Use Custom Frames",
+                default=use_custom_frames,
+                items=use_custom_frames_enum_values,
+            )
+        )
+        custom_frames_visible = cls._is_custom_frames_used(use_custom_frames)
+        defs.append(
+            TextDef(
+                "frames",
+                label="Custom Frames",
+                default="",
+                tooltip="Explicit frames to be rendered. (1001,1003-1004)(2x)",
+                visible=custom_frames_visible
+            )
+        )
+        return defs
+
+    @classmethod
+    def register_create_context_callbacks(cls, create_context):
+        create_context.add_value_changed_callback(cls.on_values_changed)
+
+    @classmethod
+    def on_values_changed(cls, event):
+        for instance_change in event["changes"]:
+            custom_frame_change = cls._get_publish_use_custom_frames_value(
+                instance_change["changes"]
+            )
+
+            instance = instance_change["instance"]
+            # recalculate only if context changes
+            if (
+                "task" not in instance_change
+                and "folderPath" not in instance_change
+                and not custom_frame_change
+            ):
+                continue
+
+            if not cls.instance_matches_plugin_families(instance):
+                continue
+
+            new_attrs = cls.get_attr_defs_for_instance(
+                event["create_context"], instance
+            )
+            instance.set_publish_plugin_attr_defs(cls.__name__, new_attrs)
+
+    @classmethod
+    def _is_custom_frames_used(cls, value) -> bool:
+        return value in ["custom_only", "reuse_last_version"]
+
+    @classmethod
+    def _get_publish_use_custom_frames_value(
+        cls,
+        instance_data
+    ) -> Optional[str]:
+        return (
+            instance_data.get("publish_attributes", {})
+                         .get("CollectCustomFrameRange", {})
+                         .get("use_custom_frames")
+        )
