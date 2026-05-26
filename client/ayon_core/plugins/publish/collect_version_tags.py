a6f13fcc91b4572adc6e1cf12d349ffa342ec560
Mustafa Zaky Jafar <mustafataherzaky@outlook.com>
2026-05-26 18:13:04 +0200

    Optimizations from code review

--- client/ayon_core/plugins/publish/collect_version_tags.py
+++ client/ayon_core/plugins/publish/collect_version_tags.py
@@ -16,7 +16,7 @@ class CollectVersionTags(
     """
 
     order = pyblish.api.CollectorOrder + 0.499
-    label = "CollectVersionTags"
+    label = "Collect Version Tags"
     settings_category = "core"
 
     enabled = False
@@ -37,16 +37,17 @@ class CollectVersionTags(
             tags = set(tags)
 
         tags.update(version_tags)
+        instance.data["versionTags"] = tags
         self.log.debug(f"Collected version tags: {tags}")
 
     @classmethod
     def get_attr_defs_for_instance(cls, create_context, instance):
-        if not cls.instance_matches_plugin_families(instance):
-            return []
-
         project_entity = create_context.get_current_project_entity()
         items = [tag["name"] for tag in project_entity["tags"]]
         if not items:
+            plugin_attributes = instance.publish_attributes.get(cls.__name__)
+            if plugin_attributes is not None:
+                plugin_attributes.pop("version_tags", None)
             return []
 
         return [
