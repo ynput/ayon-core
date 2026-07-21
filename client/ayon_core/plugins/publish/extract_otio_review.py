4e110fc1fd39f44b16cbe1220056a120639155f0
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-07-21 19:04:25 +0200

    remove 'ExtractOTIOReviewOld'

--- client/ayon_core/plugins/publish/extract_otio_review.py
+++ client/ayon_core/plugins/publish/extract_otio_review.py
@@ -28,8 +28,6 @@ from ayon_core.pipeline import (
     publish,
 )
 
-FW_KEY = "__OTIO_REVIEW__"
-
 
 class ExtractOTIOReview(
     publish.Extractor,
@@ -69,8 +67,6 @@ class ExtractOTIOReview(
             is_clip_from_media_sequence
         )
 
-        # Mark instance for 'ExtractOTIOReviewOld'
-        instance.data[FW_KEY] = True
         # TODO refactor from using instance variable
         self.temp_file_head = self._get_folder_name_based_prefix(instance)
 
@@ -657,16 +653,3 @@ class ExtractOTIOReview(
         self.log.debug(f"file_prefix::{file_prefix}")
 
         return file_prefix
-
-
-class ExtractOTIOReviewOld(ExtractOTIOReview):
-    order = ExtractOTIOReview.order + 0.00001
-    label = "Extract OTIO review (old)"
-    families = ["review"]
-    hosts = ["resolve", "hiero", "flame"]
-
-    def process(self, instance):
-        if instance.data.pop(FW_KEY, False) is True:
-            return
-        self.log.debug("Using old ExtractOTIOReview plugin")
-        super().process(instance)
