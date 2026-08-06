5bf04475d14c1defb2f28ad18bcf740d460929be
Kayla Man <moonyuet2008@hotmail.com>
2026-08-06 17:51:00 +0800

    move the CollectorOrder earlier

--- client/ayon_core/plugins/publish/collect_scene_version.py
+++ client/ayon_core/plugins/publish/collect_scene_version.py
@@ -11,7 +11,7 @@ class CollectSceneVersion(pyblish.api.ContextPlugin):
         version (int, optional): version number of the publish
     """
 
-    order = pyblish.api.CollectorOrder - 0.45
+    order = pyblish.api.CollectorOrder - 0.5
     label = "Collect Scene Version"
     # configurable in Settings
     hosts = ["*"]
