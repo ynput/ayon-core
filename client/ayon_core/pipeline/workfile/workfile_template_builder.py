aa78c94e4db7edba68732f8fcb95164fbfd79b3c
Kayla Man <moonyuet2008@hotmail.com>
2026-08-07 20:51:40 +0800

    remove log message

--- client/ayon_core/pipeline/workfile/workfile_template_builder.py
+++ client/ayon_core/pipeline/workfile/workfile_template_builder.py
@@ -729,9 +729,8 @@ class AbstractTemplateBuilder(ABC):
         ):
             preset = self.get_template_preset()
             if not preset.has_valid_path():
-                self.log.info(
-                    f"Template path '{preset.path}' does not exist. ")
-                return
+                if preset.path is None:
+                    return
             template_path: str = preset.path
             if keep_placeholders is None:
                 keep_placeholders: bool = preset.keep_placeholder
