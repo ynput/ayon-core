fcb034b34ac5aa1dd374c8389fed71feb6054e2e
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-08-27 15:29:36 +0200

    modules_from_path returns object

--- client/ayon_core/lib/python_module_tools.py
+++ client/ayon_core/lib/python_module_tools.py
@@ -1,19 +1,23 @@
 """Tools for working with python modules and classes."""
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+import importlib
+import inspect
 import os
 import sys
 import types
-from typing import Optional
-import importlib
-import inspect
-import logging
 
-log = logging.getLogger(__name__)
+from .log import Logger
+
+log = Logger.get_logger(__name__)
 
 
 def import_filepath(
-        filepath: str,
-        module_name: Optional[str] = None,
-        sys_module_name: Optional[str] = None) -> types.ModuleType:
+    filepath: str,
+    module_name: str | None = None,
+    sys_module_name: str | None = None,
+) -> types.ModuleType:
     """Import python file as python module.
 
     Args:
@@ -23,9 +27,6 @@ def import_filepath(
         sys_module_name (str): Name of module in `sys.modules` where to store
             loaded module. By default is None so module is not added to
             `sys.modules`.
-
-    Todo (antirotor): We should add the module to the sys.modules always but
-        we need to be careful about it and test it properly.
 
     """
     if module_name is None:
@@ -46,37 +47,79 @@ def import_filepath(
     return module
 
 
-def modules_from_path(folder_path):
+@dataclass
+class ModuleInfo:
+    filepath: str
+    module: types.ModuleType
+
+    def __iter__(self):
+        # Backwards compatibility - added 27/08/2026
+        # - yield data as tuple for unpacking
+        yield self.filepath
+        yield self.module
+
+
+@dataclass
+class CrashedModuleInfo:
+    filepath: str
+    exc_info: tuple
+
+    def __iter__(self):
+        # Backwards compatibility - added 27/08/2026
+        # - yield data as tuple for unpacking
+        yield self.filepath
+        yield self.exc_info
+
+
+@dataclass
+class ModulesResult:
+    modules: list[ModuleInfo] = field(default_factory=list)
+    crashed: list[CrashedModuleInfo] = field(default_factory=list)
+
+    def add_module(self, path: str, module: types.ModuleType) -> None:
+        self.modules.append(ModuleInfo(path, module))
+
+    def add_crashed_module(self, path: str, exc_info: tuple) -> None:
+        self.crashed.append(CrashedModuleInfo(path, exc_info))
+
+    def __iter__(self):
+        # Backwards compatibility - added 27/08/2026
+        # - yield data as tuple for unpacking
+        yield self.modules
+        yield self.crashed
+
+
+def modules_from_path(dir_path: str) -> ModulesResult:
     """Get python scripts as modules from a path.
 
     Arguments:
-        path (str): Path to folder containing python scripts.
+        dir_path (str): Path to folder containing python scripts.
 
     Returns:
-        tuple<list, list>: First list contains successfully imported modules
-            and second list contains tuples of path and exception.
+        ModulesResult: Contains successfully imported modules and
+            tuples of path and exception for crashed modules.
+
     """
-    crashed = []
-    modules = []
-    output = (modules, crashed)
+    result = ModulesResult()
     # Just skip and return empty list if path is not set
-    if not folder_path:
-        return output
+    if not dir_path:
+        return result
 
     # Do not allow relative imports
-    if folder_path.startswith("."):
-        log.warning((
-            "BUG: Relative paths are not allowed for security reasons. {}"
-        ).format(folder_path))
-        return output
+    if dir_path.startswith("."):
+        log.warning(
+            "BUG: Relative paths are not allowed for security reasons."
+            f" {dir_path}"
+        )
+        return result
 
-    folder_path = os.path.normpath(folder_path)
+    dir_path = os.path.normpath(dir_path)
 
-    if not os.path.isdir(folder_path):
-        log.warning("Not a directory path: {}".format(folder_path))
-        return output
+    if not os.path.isdir(dir_path):
+        log.warning(f"Not a directory path: {dir_path}")
+        return result
 
-    for filename in os.listdir(folder_path):
+    for filename in os.listdir(dir_path):
         # Ignore files which start with underscore
         if filename.startswith("_"):
             continue
@@ -85,26 +128,26 @@ def modules_from_path(folder_path):
         if not mod_ext == ".py":
             continue
 
-        full_path = os.path.join(folder_path, filename)
+        full_path = os.path.join(dir_path, filename)
         if not os.path.isfile(full_path):
             continue
 
         try:
             module = import_filepath(full_path, mod_name)
-            modules.append((full_path, module))
+            result.add_module(full_path, module)
 
         except Exception:
-            crashed.append((full_path, sys.exc_info()))
+            result.add_crashed_module(full_path, sys.exc_info())
             log.warning(
-                "Failed to load path: \"{0}\"".format(full_path),
+                f"Failed to load path: \"{full_path}\"",
                 exc_info=True
             )
             continue
 
-    return output
+    return result
 
 
-def recursive_bases_from_class(klass):
+def recursive_bases_from_class(klass: type) -> list[type]:
     """Extract all bases from entered class."""
     result = []
     bases = klass.__bases__
@@ -114,19 +157,20 @@ def recursive_bases_from_class(klass):
     return result
 
 
-def classes_from_module(superclass, module):
+def classes_from_module(
+    superclass: type, module: types.ModuleType
+) -> list[type]:
     """Return plug-ins from module
 
     Arguments:
-        superclass (superclass): Superclass of subclasses to look for
+        superclass (type): Superclass of subclasses to look for
         module (types.ModuleType): Imported module where to look for
             'superclass' subclasses.
 
     Returns:
-        List of plug-ins, or empty list if none is found.
+        list[type]: List of plug-ins, or empty list if none is found.
 
     """
-
     classes = list()
     for name in dir(module):
         # It could be anything at this point
@@ -141,7 +185,10 @@ def classes_from_module(superclass, module):
 
 
 def import_module_from_dirpath(
-        dirpath, folder_name, dst_module_name=None):
+    dirpath: str,
+    folder_name: str,
+    dst_module_name: str | None = None,
+) -> types.ModuleType:
     """Import passed directory as a python module.
 
     Imported module can be assigned as a child attribute of already loaded
@@ -162,7 +209,7 @@ def import_module_from_dirpath(
     """
     # Import passed dirpath as python module
     if dst_module_name:
-        full_module_name = "{}.{}".format(dst_module_name, folder_name)
+        full_module_name = f"{dst_module_name}.{folder_name}"
         dst_module = sys.modules[dst_module_name]
     else:
         full_module_name = folder_name
