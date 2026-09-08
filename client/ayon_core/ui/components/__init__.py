10cbbf94e7003fd114b942dba5bac57cfc227fcb
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-09-08 10:18:19 +0200

    rename spin_box to spinbox

--- client/ayon_core/ui/components/__init__.py
+++ client/ayon_core/ui/components/__init__.py
@@ -11,7 +11,7 @@ from .container import AYContainer
 from .label import AYLabel
 from .layouts import AYGridLayout, AYHBoxLayout, AYVBoxLayout
 from .line_edit import AYLineEdit
-from .spin_box import AYSpinBox
+from .spinbox import AYSpinBox
 from .option_action import (
     AYMenu,
     AYOptionalAction,
