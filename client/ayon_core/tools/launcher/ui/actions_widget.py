b3acade8adf6abb5d5e54edb0c2abc79b65355e0
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-09-10 12:12:57 +0200

    remove unused import

--- client/ayon_core/tools/launcher/ui/actions_widget.py
+++ client/ayon_core/tools/launcher/ui/actions_widget.py
@@ -16,7 +16,7 @@ from ayon_core.tools.flickcharm import FlickCharm
 from ayon_core.tools.utils import get_qt_icon
 from ayon_core.tools.attribute_defs import AttributeDefinitionsDialog
 from ayon_core.tools.launcher.abstract import WebactionContext
-from ayon_core.ui.components import AYContainer, AYLabel, AYFrame, AYGridLayout
+from ayon_core.ui.components import AYContainer, AYLabel, AYGridLayout
 from ayon_core.ui.components.scroll_area import AYScrollBar
 
 ANIMATION_LEN = 7
